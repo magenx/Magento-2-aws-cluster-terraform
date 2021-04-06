6b8a9cb891f94c895c981c64f438519ee2126f5b
MagenX <magenx@users.noreply.github.com>
2021-04-06 21:29:03 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,5 +1,9 @@
 data "aws_region" "current" {}
+
 data "aws_caller_identity" "current" {}
+
+data "aws_elb_service_account" "current" {}
+
 data "aws_availability_zones" "availability_zones" {}
 
 data "aws_acm_certificate" "issued" {
