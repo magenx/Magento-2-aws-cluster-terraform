8a4f4528d5c50b7c2c946fc79226cf72f99f279b
MagenX <magenx@users.noreply.github.com>
2021-04-19 11:54:44 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -117,7 +117,8 @@ variable "ec2_instance_profile_policy" {
   "arn:aws:iam::aws:policy/AWSCodeCommitFullAccess", 
   "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy", 
   "arn:aws:iam::aws:policy/AmazonS3FullAccess", 
-  "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+  "arn:aws:iam::aws:policy/ElasticLoadBalancingReadOnly"
   ]
 }
 
