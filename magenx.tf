5446944dc505e34e43353349ad846b8ef1f3ad7e
MagenX <magenx@users.noreply.github.com>
2021-05-13 10:01:06 +0100

    Update magenx.tf

--- magenx.tf
+++ magenx.tf
@@ -153,7 +153,7 @@ resource "aws_cloudfront_distribution" "distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = data.aws_acm_certificate.cloudfront.arn
+    acm_certificate_arn = aws_acm_certificate.cloudfront.arn
     ssl_support_method = "sni-only"
     minimum_protocol_version = "TLSv1.2_2019"
   }
