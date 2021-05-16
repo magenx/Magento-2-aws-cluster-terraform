50ad80b7c13c20a74f7b26009efda139f4f20eac
MagenX <magenx@users.noreply.github.com>
2021-05-16 23:00:37 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -68,8 +68,8 @@ variable "redis" {
   default  = {    
     node_type                  = "cache.m6g.large"
     parameter_group_name       = "default.redis6.x.cluster.on"
-    replicas_per_node_group    = "2"
-    num_node_groups            = "1"
+    replicas_per_node_group    = "1"
+    num_node_groups            = "2"
     name                       = ["session", "cache"]
   }
 }
