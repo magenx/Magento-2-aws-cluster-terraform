bcd8202bd38b5a4811d53c6aa92065834275cafa
MagenX <magenx@users.noreply.github.com>
2021-05-21 13:43:55 +0100

    Update magenx.tf

--- magenx.tf
+++ magenx.tf
@@ -1,92 +1,5 @@
 
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Create VPC
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_vpc" "this" {
-  cidr_block = "172.${local.bit}.0.0/16"
-  tags = {
-    Name = "${var.app["brand"]}-vpc"
-  }
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Create subnets for each AZ
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_subnet" "this" {
-  for_each                = data.aws_availability_zone.all
-  vpc_id                  = aws_vpc.this.id
-  availability_zone       = each.key
-  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 4, var.az_number[each.value.name_suffix])
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "${var.app["brand"]}-subnet"
-  }
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Create RDS subnet group
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_db_subnet_group" "this" {
-  name       = "${var.app["brand"]}-db-subnet"
-  description = "${var.app["brand"]}-db-subnet"
-  subnet_ids = values(aws_subnet.this).*.id
 
-  tags = {
-    Name = "${var.app["brand"]}-db-subnet"
-  }
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Create ElastiCache subnet group
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_elasticache_subnet_group" "this" {
-  description = "${var.app["brand"]}-elasticache-subnet"
-  name       = "${var.app["brand"]}-elasticache-subnet"
-  subnet_ids = values(aws_subnet.this).*.id
-  
-  tags = {
-    Name = "${var.app["brand"]}-elasticache-subnet"
-  }
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Create internet gateway - igw
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-  tags = {
-    Name = "${var.app["brand"]}-igw"
-  }
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Create route table
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_route" "this" {
-  route_table_id         = aws_vpc.this.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this.id
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Assign AZ subnets to route table
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_route_table_association" "this" {
-  for_each       = aws_subnet.this
-  subnet_id      = aws_subnet.this[each.key].id
-  route_table_id = aws_vpc.this.main_route_table_id
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Create DHCP options
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_vpc_dhcp_options" "this" {
-  domain_name          = "${data.aws_region.current.name}.compute.internal"
-  domain_name_servers  = ["AmazonProvidedDNS"]
-  tags = {
-    Name = "${var.app["brand"]}-dhcp"
-  }
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Assign DHCP options to VPC
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_vpc_dhcp_options_association" "this" {
-  vpc_id          = aws_vpc.this.id
-  dhcp_options_id = aws_vpc_dhcp_options.this.id
-}
 
 ///////////////////////////////////////////////////[ RANDOM STRING GENERATOR ]////////////////////////////////////////////
 
@@ -116,6 +29,97 @@ resource "random_string" "string" {
   number         = true
   special        = false
   upper          = false
+}
+
+
+
+////////////////////////////////////////////////////////[ VPC NETWORKING ]////////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create our dedicated VPC
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_vpc" "this" {
+  cidr_block = "172.${local.bit}.0.0/16"
+  tags = {
+    Name = "${var.app["brand"]}-vpc"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create subnets for each AZ in our dedicated VPC
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_subnet" "this" {
+  for_each                = data.aws_availability_zone.all
+  vpc_id                  = aws_vpc.this.id
+  availability_zone       = each.key
+  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 4, var.az_number[each.value.name_suffix])
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.app["brand"]}-subnet"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create RDS subnet group in our dedicated VPC
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_db_subnet_group" "this" {
+  name       = "${var.app["brand"]}-db-subnet"
+  description = "${var.app["brand"]}-db-subnet"
+  subnet_ids = values(aws_subnet.this).*.id
+  tags = {
+    Name = "${var.app["brand"]}-db-subnet"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create ElastiCache subnet group in our dedicated VPC
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_elasticache_subnet_group" "this" {
+  description = "${var.app["brand"]}-elasticache-subnet"
+  name       = "${var.app["brand"]}-elasticache-subnet"
+  subnet_ids = values(aws_subnet.this).*.id 
+  tags = {
+    Name = "${var.app["brand"]}-elasticache-subnet"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create internet gateway in our dedicated VPC
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "${var.app["brand"]}-igw"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create route table in our dedicated VPC
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_route" "this" {
+  route_table_id         = aws_vpc.this.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Assign AZ subnets to route table in our dedicated VPC
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_route_table_association" "this" {
+  for_each       = aws_subnet.this
+  subnet_id      = aws_subnet.this[each.key].id
+  route_table_id = aws_vpc.this.main_route_table_id
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create DHCP options in our dedicated VPC
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_vpc_dhcp_options" "this" {
+  domain_name          = "${data.aws_region.current.name}.compute.internal"
+  domain_name_servers  = ["AmazonProvidedDNS"]
+  tags = {
+    Name = "${var.app["brand"]}-dhcp"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Assign DHCP options to our dedicated VPC
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_vpc_dhcp_options_association" "this" {
+  vpc_id          = aws_vpc.this.id
+  dhcp_options_id = aws_vpc_dhcp_options.this.id
 }
 
 
