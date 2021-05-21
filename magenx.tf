1d26a4aca4ce1cc0a6f98a8af0f4b1ed3fe4c8d4
MagenX <magenx@users.noreply.github.com>
2021-05-21 17:09:39 +0100

    Update magenx.tf

--- magenx.tf
+++ magenx.tf
@@ -39,7 +39,10 @@ resource "random_string" "string" {
 # Create our dedicated VPC
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_vpc" "this" {
-  cidr_block = "172.${local.bit}.0.0/16"
+  cidr_block           = "172.${local.bit}.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
     Name = "${var.app["brand"]}-vpc"
   }
