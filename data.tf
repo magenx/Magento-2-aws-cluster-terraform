40cf2a61d31fd8ebac49697298dfd941bd0e7c04
MagenX <magenx@users.noreply.github.com>
2021-06-06 18:17:33 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -109,8 +109,10 @@ GITHUB_REPO_API_URL = "https://api.github.com/repos/magenx/Magento-2-aws-cluster
 
 ALB_DNS_NAME = "${aws_lb.this["inner"].dns_name}"
 EFS_DNS_TARGET = "${values(aws_efs_mount_target.this).0.dns_name}"
-CODECOMMIT_APP_REPO_NAME = "${aws_codecommit_repository.this.repository_name}"
-
+  
+CODECOMMIT_APP_REPO = "codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.app.repository_name}"
+CODECOMMIT_SERVICES_REPO = "codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.services.repository_name}"
+  
 EXTRA_PACKAGES_DEB = "curl jq nfs-common gnupg2 apt-transport-https apt-show-versions ca-certificates lsb-release unzip vim wget git patch python3-pip acl attr imagemagick snmp"
 PHP_PACKAGES_DEB = "cli fpm json common mysql zip gd mbstring curl xml bcmath intl soap oauth lz4"
 
