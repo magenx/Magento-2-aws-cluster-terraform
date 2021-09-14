e512399b1cd027541dcae2ff9da3004bdd4592b9
MagenX <magenx@users.noreply.github.com>
2021-09-14 14:11:04 +0100

    Update magenx.tf

--- magenx.tf
+++ magenx.tf
@@ -980,7 +980,7 @@ resource "aws_lb_target_group" "this" {
   protocol    = "HTTP"
   vpc_id      = aws_vpc.this.id
   health_check {
-    path = "${var.app["brand"]}-${each.key}-health-check"
+    path = "/${var.app["brand"]}-${each.key}-health-check"
   }
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
