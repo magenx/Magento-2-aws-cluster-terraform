50d15b81064230eeb2c1547203350b11c2514644
MagenX <magenx@users.noreply.github.com>
2021-10-06 12:50:57 +0100

    Update magenx.tf

--- magenx.tf
+++ magenx.tf
@@ -1250,7 +1250,7 @@ resource "aws_launch_template" "this" {
   block_device_mappings {
     device_name = "/dev/xvda"
     ebs { 
-        volume_size = "50"
+        volume_size = var.app["volume_size"]
         volume_type = "gp3"
             }
   }
