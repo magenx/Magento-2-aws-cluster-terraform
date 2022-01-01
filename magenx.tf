9359cda86e868fdbe3e39e3d2a055259294bf524
MagenX <magenx@users.noreply.github.com>
2022-01-01 18:42:26 +0000

    Update magenx.tf

--- magenx.tf
+++ magenx.tf
@@ -1408,27 +1408,30 @@ SECURITY_GROUP=${aws_security_group.ec2.id}
 
 SOURCE_AMI=${data.aws_ami.distro.id}
 VOLUME_SIZE=${var.app["volume_size"]}
-
-RESOLVER=${cidrhost(aws_vpc.this.cidr_block, 2)}
 								   											
 ALB_DNS_NAME=${aws_lb.this.dns_name}
 EFS_DNS_TARGET=${values(aws_efs_mount_target.this).0.dns_name}
 SNS_TOPIC_ARN=${aws_sns_topic.default.arn}
+
 CODECOMMIT_APP_REPO=codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.app.repository_name}
 CODECOMMIT_SERVICES_REPO=codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.services.repository_name}
+
 RABBITMQ_ENDPOINT=${regex("amqps://(.*):5671",aws_mq_broker.this.instances.0.endpoints.0)}
 RABBITMQ_USER=${var.app["brand"]}
 RABBITMQ_PASSWORD=${random_password.this["rabbitmq"].result}
+
 ELASTICSEARCH_ENDPOINT=https://${aws_elasticsearch_domain.this.endpoint}:443
-REDIS_CACHE_BACKEND=${aws_elasticache_replication_group.this.primary_endpoint_address}
+
+REDIS_CACHE_BACKEND=${aws_elasticache_replication_group.this["cache"].primary_endpoint_address}
+REDIS_SESSION_BACKEND=${aws_elasticache_replication_group.this["session"].primary_endpoint_address}
 
 SES_KEY=${aws_iam_access_key.ses_smtp_user_access_key.id}
 SES_SECRET=${aws_iam_access_key.ses_smtp_user_access_key.secret}
 SES_PASSWORD=${aws_iam_access_key.ses_smtp_user_access_key.ses_smtp_password_v4}
 
-DATABASE_ENDPOINT=${aws_db_instance.this.endpoint}
-DATABASE_INSTANCE_NAME=${aws_db_instance.this.name}
-DATABASE_USER_NAME=${aws_db_instance.this.username}
+DATABASE_ENDPOINT=${aws_db_instance.this["production"].endpoint}
+DATABASE_INSTANCE_NAME=${aws_db_instance.this["production"].name}
+DATABASE_USER_NAME=${aws_db_instance.this["production"].username}
 DATABASE_PASSWORD=${random_password.this["rds"].result}
 
 ADMIN_PATH=admin_${random_string.this["admin_path"].result}
