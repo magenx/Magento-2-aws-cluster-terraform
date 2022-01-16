2d81fc78bf141bba3a4cb2b66684280e2346c68f
MagenX <magenx@users.noreply.github.com>
2022-01-16 17:38:53 +0000

    Create packer.tf

---
+++ packer.tf
@@ -0,0 +1,28 @@
+
+
+
+//////////////////////////////////////////////////////[ PACKER BUILDER ]//////////////////////////////////////////////////
+
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create custom AMI with Packer Builder
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "null_resource" "packer" {
+  for_each = var.ec2
+  provisioner "local-exec" {
+    working_dir = "${abspath(path.root)}/packer"
+    command = <<EOF
+PACKER_LOG=1 PACKER_LOG_PATH="packerlog" /usr/bin/packer build \
+-var INSTANCE_NAME=${each.key} \
+-var IAM_INSTANCE_PROFILE=${aws_iam_instance_profile.ec2[each.key].name} \
+-var PARAMETERSTORE_NAME=${aws_ssm_parameter.env.name} \
+packer.pkr.hcl
+EOF
+  }
+ triggers = {
+    ami_creation_date = data.aws_ami.distro.creation_date
+    build_script      = filesha256("${abspath(path.root)}/packer/build.sh")
+  }
+}
+
+
