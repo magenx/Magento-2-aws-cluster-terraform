173860195d05241d0f16d898cd975dea884cc696
MagenX <magenx@users.noreply.github.com>
2022-01-16 17:52:18 +0000

    Create ec2_autoscaling.tf

---
+++ ec2_autoscaling.tf
@@ -0,0 +1,147 @@
+
+
+
+/////////////////////////////////////////////////////[ AUTOSCALING CONFIGURATION ]////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create Launch Template for Autoscaling Groups - user_data converted
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_launch_template" "this" {
+  for_each = var.ec2
+  name = "${var.app["brand"]}-${each.key}-ltpl"
+  iam_instance_profile { name = aws_iam_instance_profile.ec2[each.key].name }
+  image_id = element(values(data.external.packer[each.key].result), 0)
+  instance_type = each.value
+  monitoring { enabled = false }
+  network_interfaces { 
+    associate_public_ip_address = true
+    security_groups = [aws_security_group.this["ec2"].id]
+  }
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.app["brand"]}-${each.key}-ec2" }
+  }
+  tag_specifications {
+    resource_type = "volume"
+    tags = {
+      Name = "${var.app["brand"]}-${each.key}-ec2" }
+  }
+  user_data = base64encode(data.template_file.user_data[each.key].rendered)
+  update_default_version = true
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create Autoscaling Groups
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_autoscaling_group" "this" {
+  for_each = var.ec2
+  name = "${var.app["brand"]}-${each.key}-asg"
+  vpc_zone_identifier = values(aws_subnet.this).*.id
+  desired_capacity    = var.asg["desired_capacity"]
+  min_size            = var.asg["min_size"]
+  max_size            = var.asg["max_size"]
+  health_check_grace_period = var.asg["health_check_grace_period"]
+  health_check_type         = var.asg["health_check_type"]
+  target_group_arns  = [aws_lb_target_group.this[each.key].arn]
+  dynamic "warm_pool" {
+    for_each = var.asg["warm_pool"] == "enabled" ? [var.ec2] : []
+    content {
+      pool_state                  = "Stopped"
+      min_size                    = var.asg["min_size"]
+      max_group_prepared_capacity = var.asg["max_size"]
+    }
+  }
+  launch_template {
+    name    = aws_launch_template.this[each.key].name
+    version = "$Latest"
+  }
+  instance_refresh {
+     strategy = "Rolling"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create Autoscaling groups actions for SNS topic email alerts
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_autoscaling_notification" "this" {
+for_each = aws_autoscaling_group.this 
+group_names = [
+    aws_autoscaling_group.this[each.key].name
+  ]
+
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+    "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
+  ]
+
+  topic_arn = aws_sns_topic.default.arn
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create Autoscaling policy for scale-out
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_autoscaling_policy" "scaleout" {
+  for_each               = var.ec2
+  name                   = "${var.app["brand"]}-${each.key}-asp-out"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.this[each.key].name
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CloudWatch alarm metric to execute Autoscaling policy for scale-out
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_metric_alarm" "scaleout" {
+  for_each            = var.ec2
+  alarm_name          = "${var.app["brand"]}-${each.key} scale-out alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = var.asp["evaluation_periods"]
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = var.asp["period"]
+  statistic           = "Average"
+  threshold           = var.asp["out_threshold"]
+  dimensions = {
+    AutoScalingGroupName  = aws_autoscaling_group.this[each.key].name
+  }
+  alarm_description = "${each.key} scale-out alarm - CPU exceeds ${var.asp["out_threshold"]} percent"
+  alarm_actions     = [aws_autoscaling_policy.scaleout[each.key].arn]
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create Autoscaling policy for scale-in
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_autoscaling_policy" "scalein" {
+  for_each               = var.ec2
+  name                   = "${var.app["brand"]}-${each.key}-asp-in"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.this[each.key].name
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CloudWatch alarm metric to execute Autoscaling policy for scale-in
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_metric_alarm" "scalein" {
+  for_each            = var.ec2
+  alarm_name          = "${var.app["brand"]}-${each.key} scale-in alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = var.asp["evaluation_periods"]
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = var.asp["period"]
+  statistic           = "Average"
+  threshold           = var.asp["in_threshold"]
+  dimensions = {
+    AutoScalingGroupName  = aws_autoscaling_group.this[each.key].name
+  }
+  alarm_description = "${each.key} scale-in alarm - CPU less than ${var.asp["in_threshold"]} percent"
+  alarm_actions     = [aws_autoscaling_policy.scalein[each.key].arn]
+}
+
+
