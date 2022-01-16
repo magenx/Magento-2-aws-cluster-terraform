46e14427bc6fe03494432df4ccac20cbd2a9a044
MagenX <magenx@users.noreply.github.com>
2022-01-16 17:40:56 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,37 @@
-provider "aws" {}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 2.0"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "null" {}
+provider "random" {}
+provider "template" {}
+provider "external" {}
+
 provider "aws" {
   alias  = "useast1"
   region = "us-east-1"
