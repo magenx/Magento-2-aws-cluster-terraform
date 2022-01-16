5543e5386ebf40105d5924b205d505a090cce601
MagenX <magenx@users.noreply.github.com>
2022-01-16 18:03:39 +0000

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,214 @@
+
+
+
+/////////////////////////////////////////////////[ APPLICATION LOAD BALANCER ]////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create Application Load Balancers
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lb" "this" {
+  for_each           = toset(var.alb["type"])
+  name               = "${var.app["brand"]}-${each.key}-alb"
+  internal           = (each.key == "inner" ? true : false)
+  load_balancer_type = "application"
+  drop_invalid_header_fields = true
+  security_groups    = [aws_security_group.this[each.key].id]
+  subnets            = values(aws_subnet.this).*.id
+  access_logs {
+    bucket  = aws_s3_bucket.this["system"].bucket
+    prefix  = "${var.app["brand"]}-alb"
+    enabled = true
+  }
+  tags = {
+    Name = "${var.app["brand"]}-${each.key}-alb"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create Target Groups for Load Balancers
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lb_target_group" "this" {
+  for_each    = var.ec2
+  name        = "${var.app["brand"]}-${each.key}-target"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.this.id
+  health_check {
+    path = "/${random_string.this["health_check"].result}"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create https:// listener for OUTER Load Balancer - forward to varnish
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lb_listener" "outerhttps" {
+  depends_on = [aws_acm_certificate_validation.default]
+  load_balancer_arn = aws_lb.this["outer"].arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-FS-1-2-Res-2020-10"
+  certificate_arn   = aws_acm_certificate.default.arn
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this["varnish"].arn
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create http:// listener for OUTER Load Balancer - redirect to https://
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lb_listener" "outerhttp" {
+  load_balancer_arn = aws_lb.this["outer"].arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create default listener for INNER Load Balancer - default response
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lb_listener" "inner" {
+  load_balancer_arn = aws_lb.this["inner"].arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+        content_type = "text/plain"
+        message_body = "No targets are responding to this request"
+        status_code  = "502"
+        }
+    }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create conditional listener rule for INNER Load Balancer - forward to frontend
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lb_listener_rule" "innerfrontend" {
+  listener_arn = aws_lb_listener.inner.arn
+  priority     = 30
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this["frontend"].arn
+  }
+  condition {
+    host_header {
+      values = [var.app["domain"]]
+    }
+  }
+  condition {
+    http_header {
+      http_header_name = "X-Magenx-Header"
+      values           = [random_uuid.this.result]
+    }
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create conditional listener rule for INNER Load Balancer - forward to admin
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lb_listener_rule" "inneradmin" {
+  listener_arn = aws_lb_listener.inner.arn
+  priority     = 20
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this["admin"].arn
+  }
+  condition {
+    http_header {
+      http_header_name = "X-Magenx-Header"
+      values           = [random_uuid.this.result]
+    }
+  }
+  condition {
+    path_pattern {
+      values = ["/admin_${random_string.this["admin_path"].result}/*"]
+    }
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create conditional listener rule for INNER Load Balancer - forward to phpmyadmin
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lb_listener_rule" "innermysql" {
+  listener_arn = aws_lb_listener.inner.arn
+  priority     = 10
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this["admin"].arn
+  }
+  condition {
+    http_header {
+      http_header_name = "X-Magenx-Header"
+      values           = [random_uuid.this.result]
+    }
+  }
+  condition {
+    path_pattern {
+      values = ["/mysql_${random_string.this["mysql_path"].result}/*"]
+    }
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CloudWatch HTTP 5XX metrics and email alerts
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_metric_alarm" "httpcode_target_5xx_count" {
+  alarm_name          = "${var.app["brand"]}-http-5xx-errors-from-target"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "HTTPCode_Target_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = var.alb["error_threshold"]
+  alarm_description   = "HTTPCode 5XX count for frontend instances over ${var.alb["error_threshold"]}"
+  alarm_actions       = ["${aws_sns_topic.default.arn}"]
+  ok_actions          = ["${aws_sns_topic.default.arn}"]
+  
+  dimensions = {
+    TargetGroup  = aws_lb_target_group.this["frontend"].arn
+    LoadBalancer = aws_lb.this["inner"].arn
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CloudWatch HTTP 5XX metrics and email alerts
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_metric_alarm" "httpcode_elb_5xx_count" {
+  alarm_name          = "${var.app["brand"]}-http-5xx-errors-from-loadbalancer"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "HTTPCode_ELB_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = var.alb["error_threshold"]
+  alarm_description   = "HTTPCode 5XX count for loadbalancer over ${var.alb["error_threshold"]}"
+  alarm_actions       = ["${aws_sns_topic.default.arn}"]
+  ok_actions          = ["${aws_sns_topic.default.arn}"]
+  
+  dimensions = {
+    LoadBalancer = aws_lb.this["outer"].arn
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CloudWatch RequestCount metrics and email alerts
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_metric_alarm" "alb_rps" {
+  alarm_name          = "${var.app["brand"]}-loadbalancer-rps"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "RequestCount"
+  namespace           = "AWS/ApplicationELB"
+  period              = "120"
+  statistic           = "Sum"
+  threshold           = var.alb["rps_threshold"]
+  alarm_description   = "The number of requests processed over 2 minutes greater than ${var.alb["rps_threshold"]}"
+  alarm_actions       = ["${aws_sns_topic.default.arn}"]
+  ok_actions          = ["${aws_sns_topic.default.arn}"]
+
+  dimensions = {
+    LoadBalancer = aws_lb.this["outer"].arn
+  }
+}
+
+
