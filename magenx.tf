d764c6445b4bfe41247afc1a40476195b1df1fa9
MagenX <magenx@users.noreply.github.com>
2022-01-25 21:29:23 +0000

    Update magenx.tf

--- magenx.tf
+++ magenx.tf
@@ -493,10 +493,11 @@ resource "aws_s3_bucket" "this" {
 # Block public access options for S3 bucket
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_s3_bucket_public_access_block" "this" {
-  for_each                = aws_s3_bucket.this
+  for_each                = toset(values(aws_s3_bucket.this).*.id)
   bucket                  = each.key
   block_public_acls       = true
   block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
