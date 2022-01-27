a7dd2ab2608175090399f4efea1f291a4d94e2ca
MagenX <magenx@users.noreply.github.com>
2022-01-27 18:15:29 +0000

    Update and rename magenx.tf to aws_budget.tf

---
+++ aws_budget.tf
@@ -0,0 +1,24 @@
+
+
+
+/////////////////////////////////////////////////[ AWS BUDGET NOTIFICATION ]//////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create alert when your budget thresholds are forecasted to exceed
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_budgets_budget" "all" {
+  name              = "${local.project}-budget-monthly-forecasted"
+  budget_type       = "COST"
+  limit_amount      = "2000"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = [var.app["admin_email"]]
+  }
+}
+
