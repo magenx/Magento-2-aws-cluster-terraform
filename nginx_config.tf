3ebd84010c9727fed8a54bed5e5e564325a0a70b
MagenX <magenx@users.noreply.github.com>
2022-01-28 12:45:52 +0000

    Update nginx_config.tf

--- nginx_config.tf
+++ nginx_config.tf
@@ -66,4 +66,64 @@ run_command_targets {
   }
 }
       
+            
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create SSM Document runShellScript to pull varnish configuration from CodeCommit
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_ssm_document" "codecommit_varnish" {
+  name            = "${local.project}-codecommit-pull-varnish-config-changes"
+  document_type   = "Command"
+  document_format = "YAML"
+  target_type     = "/AWS::EC2::Instance"
+  content = <<EOT
+---
+schemaVersion: "2.2"
+description: "Pull varnish configuration changes from CodeCommit"
+parameters:
+mainSteps:
+- action: "aws:runShellScript"
+  name: "${var.app["brand"]}CodeCommitPullVarnishConfigChanges"
+  inputs:
+    runCommand:
+    - |-
+      #!/bin/bash
+      cd /etc/varnish
+      git fetch origin varnish
+      git reset --hard origin/varnish
+      git checkout -t origin/varnish
+      if varnishd -Cf /etc/varnish/default.vcl 2>/dev/null; then systemctl restart varnish; else exit 1; fi
+EOT
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create EventBridge rule to monitor CodeCommit varnish branch state
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_event_rule" "codecommit_varnish" {
+  name          = "${local.project}-Varnish-Repo-State"
+  description   = "CloudWatch monitor varnish repository state change"
+  event_pattern = <<EOF
+{
+	"source": ["aws.codecommit"],
+	"detail-type": ["CodeCommit Repository State Change"],
+	"resources": ["${aws_codecommit_repository.services.arn}"],
+	"detail": {
+		"referenceType": ["branch"],
+		"referenceName": ["varnish"]
+	}
+}
+EOF
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create EventBridge target to execute SSM Document
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_event_target" "codecommit_varnish" {
+  rule      = aws_cloudwatch_event_rule.codecommit_varnish.name
+  target_id = "${local.project}-Varnish-Config-Deployment-Script"
+  arn       = aws_ssm_document.codecommit_varnish.arn
+  role_arn  = aws_iam_role.eventbridge_service_role.arn
+ 
+run_command_targets {
+    key    = "tag:Name"
+    values = [aws_launch_template.this["frontend"].tag_specifications[0].tags.Name]
+  }
+}
       
