859e2c307cf6af7045d1497b4a93e64cb7983fb3
MagenX <magenx@users.noreply.github.com>
2022-02-01 12:49:47 +0000

    Update rabbitmq.tf

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -13,7 +13,7 @@ resource "aws_mq_broker" "this" {
   host_instance_type = var.rabbitmq["host_instance_type"]
   security_groups    = [aws_security_group.rabbitmq.id]
   deployment_mode    = "ACTIVE_STANDBY_MULTI_AZ"
-  subnet_ids         = [values(aws_subnet.this).*.id]
+  subnet_ids         = values(aws_subnet.this).*.id
   user {
     username = var.app["brand"]
     password = random_password.this["rabbitmq"].result
