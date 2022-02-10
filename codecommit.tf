b601a8c4b684bf99f255bea01a9371f4f023a341
MagenX <magenx@users.noreply.github.com>
2022-02-10 17:08:38 +0000

    Update codecommit.tf

--- codecommit.tf
+++ codecommit.tf
@@ -21,6 +21,7 @@ resource "aws_codecommit_repository" "app" {
           git config --global user.email "${var.app["admin_email"]}"
           git remote set-url origin codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.app.repository_name}
           git commit --allow-empty -m "init"
+          git branch -m main
           git push origin main
           git checkout -b build
           git push origin build
