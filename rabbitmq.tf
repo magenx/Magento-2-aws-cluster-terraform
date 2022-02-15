a8c077b8e181cdd8f916cd17aa4fd2be96a6e581
MagenX <magenx@users.noreply.github.com>
2022-02-15 11:19:31 +0000

    Update rabbitmq.tf

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -7,18 +7,18 @@
 # Create RabbitMQ - queue message broker
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_mq_broker" "this" {
-  broker_name = "${local.project}-${var.rabbitmq["broker_name"]}"
+  broker_name        = "${local.project}-rabbitmq"
   engine_type        = "RabbitMQ"
   engine_version     = var.rabbitmq["engine_version"]
   host_instance_type = var.rabbitmq["host_instance_type"]
   security_groups    = [aws_security_group.rabbitmq.id]
   subnet_ids         = [values(aws_subnet.this).0.id]
   user {
-    username = var.app["brand"]
-    password = random_password.this["rabbitmq"].result
+    username         = var.app["brand"]
+    password         = random_password.this["rabbitmq"].result
   }
   tags = {
-    Name   = "${local.project}-${var.rabbitmq["broker_name"]}"
+    Name   = "${local.project}-rabbitmq"
   }
 }
 
