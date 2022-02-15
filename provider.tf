f160dc09a31cd1b7a1ca1ef9d8770f03e70a3353
MagenX <magenx@users.noreply.github.com>
2022-02-15 14:10:18 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
     null = {
       source  = "hashicorp/null"
