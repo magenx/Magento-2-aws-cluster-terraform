67bbb78629be9b3b0e6d492c74fef19eb9cb6a87
MagenX <magenx@users.noreply.github.com>
2022-02-15 09:48:39 +0000

    Create acm.tf

---
+++ acm.tf
@@ -0,0 +1,23 @@
+
+
+
+///////////////////////////////////////////////////[ AWS CERTIFICATE MANAGER ]////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create and validate ssl certificate for domain and subdomains
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_acm_certificate" "default" {
+  domain_name               = "${var.app["domain"]}"
+  subject_alternative_names = ["*.${var.app["domain"]}"]
+  validation_method         = "EMAIL"
+
+lifecycle {
+    create_before_destroy   = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "default" {
+  certificate_arn = aws_acm_certificate.default.arn
+}
+
+
