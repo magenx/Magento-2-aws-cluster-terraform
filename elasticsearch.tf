12c1bce375f43d37c34ff9376707cf3cf83e9b7b
MagenX <magenx@users.noreply.github.com>
2022-02-15 11:28:58 +0000

    Update elasticsearch.tf

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -22,16 +22,21 @@ EOF
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_elasticsearch_domain" "this" {
   depends_on = [null_resource.es]
-  domain_name           = "${local.project}-${var.elk["domain_name"]}"
+  domain_name           = "${local.project}-elk"
   elasticsearch_version = var.elk["elasticsearch_version"]
   cluster_config {
     instance_type  = var.elk["instance_type"]
     instance_count = var.elk["instance_count"]
-    
-    zone_awareness_enabled = true
-    zone_awareness_config {
+  zone_awareness_enabled = var.elk["instance_count"] > 1 ? true : false
+  dynamic "zone_awareness_config" {
+     for_each = var.elk["instance_count"] > 1 ? [var.elk["instance_count"]] : []
+     content {
         availability_zone_count = var.elk["instance_count"]
       }
+    }
+  }
+  encrypt_at_rest {
+    enabled = true
   }
   ebs_options {
     ebs_enabled = var.elk["ebs_enabled"]
@@ -43,7 +48,7 @@ resource "aws_elasticsearch_domain" "this" {
     security_group_ids = [aws_security_group.elk.id]
   }
   tags = {
-    Name = "${local.project}-${var.elk["domain_name"]}"
+    Name = "${local.project}-elk"
   }
   log_publishing_options {
     cloudwatch_log_group_arn = aws_cloudwatch_log_group.elk.arn
@@ -63,7 +68,7 @@ resource "aws_elasticsearch_domain" "this" {
       "Action": [
         "es:*"
       ],
-      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.project}-${var.elk["domain_name"]}/*"
+      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.project}-elk/*"
     }
   ]
 }
@@ -73,11 +78,11 @@ EOF
 # Create CloudWatch log group for ElasticSearch log stream
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_cloudwatch_log_group" "elk" {
-  name = "${local.project}-${var.elk["domain_name"]}"
+  name = "${local.project}-elk"
 }
 
 resource "aws_cloudwatch_log_resource_policy" "elk" {
-  policy_name = "${local.project}-${var.elk["domain_name"]}"
+  policy_name = "${local.project}-elk"
 
   policy_document = <<EOF
 {
