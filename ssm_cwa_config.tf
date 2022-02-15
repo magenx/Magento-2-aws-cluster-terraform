77f80ea9d71080b15d303f3c58aeef680750bea6
MagenX <magenx@users.noreply.github.com>
2022-02-15 11:38:47 +0000

    Update ssm_cwa_config.tf

--- ssm_cwa_config.tf
+++ ssm_cwa_config.tf
@@ -9,7 +9,7 @@
 resource "aws_ssm_parameter" "cloudwatch_agent_config" {
   for_each    = var.ec2
   name        = "amazon-cloudwatch-agent-${each.key}.json"
-  description = "Configuration file for CloudWatch agent at ${each.key}"
+  description = "Configuration file for CloudWatch agent at ${each.key} for ${local.project}"
   type        = "String"
   value       = <<EOF
 {
@@ -19,40 +19,40 @@ resource "aws_ssm_parameter" "cloudwatch_agent_config" {
             "collect_list": [
             {
                 "file_path": "/var/log/nginx/error.log",
-                "log_group_name": "${var.app["brand"]}_nginx_error_logs",
+                "log_group_name": "${local.project}_nginx_error_logs",
                 "log_stream_name": "${each.key}-{instance_id}-{ip_address}"
             },
             %{ if each.key == "admin" ~}
             {
                 "file_path": "/home/${var.app["brand"]}/public_html/var/log/php-fpm-error.log",
-                "log_group_name": "${var.app["brand"]}_php_app_error_logs",
+                "log_group_name": "${local.project}_php_app_error_logs",
                 "log_stream_name": "${each.key}-{instance_id}-{ip_address}"
             },
             {
                 "file_path": "/home/${var.app["brand"]}/public_html/var/log/exception.log",
-                "log_group_name": "${var.app["brand"]}_app_error_logs",
+                "log_group_name": "${local.project}_app_error_logs",
                 "log_stream_name": "${each.key}-{instance_id}-{ip_address}"
             },
             %{ endif ~}
             {
                 "file_path": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log",
-                "log_group_name": "${var.app["brand"]}_cloudwatch_agent_log",
+                "log_group_name": "${local.project}_cloudwatch_agent_log",
                 "log_stream_name": "${each.key}-{instance_id}-{ip_address}"
             },
             {
                 "file_path": "/var/log/apt/history.log",
-                "log_group_name": "${var.app["brand"]}_system_apt_history",
+                "log_group_name": "${local.project}_system_apt_history",
                 "log_stream_name": "${each.key}-{instance_id}-{ip_address}"
             },
             {
                 "file_path": "/var/log/syslog",
-                "log_group_name": "${var.app["brand"]}_system_syslog",
+                "log_group_name": "${local.project}_system_syslog",
                 "log_stream_name": "${each.key}-{instance_id}-{ip_address}"
             }
             ]
           }
         },
-        "log_stream_name": "${var.app["domain"]}",
+        "log_stream_name": "${local.project}",
         "force_flush_interval" : 60
       }
 }
