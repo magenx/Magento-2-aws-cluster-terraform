60f7e9544bf7f563e8df2b47c14fb255242b33f6
MagenX <magenx@users.noreply.github.com>
2022-02-15 10:03:09 +0000

    Update codepipeline.tf

--- codepipeline.tf
+++ codepipeline.tf
@@ -416,7 +416,7 @@ resource "aws_codepipeline" "this" {
       configuration = {
                 BucketName = aws_s3_bucket.this["backup"].bucket
                 Extract    = false
-                ObjectKey  = "deploy/{datetime}/${local.project}.zip"
+                ObjectKey  = "deploy/${local.project}.zip"
                 }
       input_artifacts = [
         "BuildArtifact",
@@ -496,3 +496,82 @@ run_command_targets {
     values = [aws_launch_template.this["admin"].tag_specifications[0].tags.Name,aws_launch_template.this["frontend"].tag_specifications[0].tags.Name]
   }
 }
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create EventBridge service role
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_role" "eventbridge_service_role" {
+  name = "${local.project}-EventBridgeServiceRole"
+  description = "Provides EventBridge manage events on your behalf."
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "events.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create policy for EventBridge role to start CodePipeline
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_policy" "eventbridge_service_role" {
+  name = "${local.project}-start-codepipeline"
+  path = "/service-role/"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "codepipeline:StartPipelineExecution"
+            ],
+            "Resource": [
+                "${aws_codepipeline.this.arn}"
+            ]
+        }
+    ]
+}
+EOF
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Attach policies to EventBridge role
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_role_policy_attachment" "eventbridge_service_role" {
+  for_each   = var.eventbridge_policy
+  role       = aws_iam_role.eventbridge_service_role.name
+  policy_arn = each.value
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create EventBridge rule to run Magento cronjob
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_event_rule" "cronjob" {
+  name        = "${local.project}-EventBridge-Rule-Run-Magento-Cronjob"
+  description = "EventBridge rule to run Magento cronjob every minute"
+  schedule_expression = "rate(1 minute)"
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create EventBridge target to execute SSM command
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_event_target" "cronjob" {
+  rule      = aws_cloudwatch_event_rule.cronjob.name
+  target_id = "${local.project}-EventBridge-Target-Admin-Instance-Cron"
+  arn       = "arn:aws:ssm:${data.aws_region.current.name}::document/AWS-RunShellScript"
+  role_arn  = aws_iam_role.eventbridge_service_role.arn
+  input     = "{\"commands\":[\"su ${var.app["brand"]} -s /bin/bash -c '/home/${var.app["brand"]}/public_html/bin/magento cron:run 2>&1'\"],\"executionTimeout\":[\"180\"]}"
+ 
+run_command_targets {
+    key    = "tag:Name"
+    values = [aws_launch_template.this["admin"].tag_specifications[0].tags.Name]
+  }
+}
+
+
