824abd2b5a258c73d844878ef86319535943fb52
MagenX <magenx@users.noreply.github.com>
2022-02-21 22:17:45 +0000

    Update elasticache.tf

--- elasticache.tf
+++ elasticache.tf
@@ -35,19 +35,19 @@ resource "aws_elasticache_parameter_group" "this" {
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_elasticache_replication_group" "this" {
   for_each                      = toset(var.redis["name"])
-  number_cache_clusters         = var.redis["number_cache_clusters"]
+  description                   = "Replication group for ${var.app["domain"]} ${each.key} backend"
+  num_cache_clusters            = var.redis["num_cache_clusters"]
   at_rest_encryption_enabled    = var.redis["at_rest_encryption_enabled"]
   engine                        = "redis"
   engine_version                = var.redis["engine_version"]
   replication_group_id          = "${local.project}-${each.key}-backend"
-  replication_group_description = "Replication group for ${var.app["domain"]} ${each.key} backend"
   node_type                     = var.redis["node_type"]
   port                          = var.redis["port"]
   parameter_group_name          = aws_elasticache_parameter_group.this[each.key].id
   security_group_ids            = [aws_security_group.redis.id]
   subnet_group_name             = aws_elasticache_subnet_group.this.name
-  automatic_failover_enabled    = var.redis["number_cache_clusters"] > 1 ? true : false
-  multi_az_enabled              = var.redis["number_cache_clusters"] > 1 ? true : false
+  automatic_failover_enabled    = var.redis["num_cache_clusters"] > 1 ? true : false
+  multi_az_enabled              = var.redis["num_cache_clusters"] > 1 ? true : false
   notification_topic_arn        = aws_sns_topic.default.arn
   tags = {
     Name = "${local.project}-${each.key}-backend"
