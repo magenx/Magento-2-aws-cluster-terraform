c8c821096c346cccecd9a76cfa49c9fa8b04f7f6
MagenX <magenx@users.noreply.github.com>
2022-03-20 10:31:26 +0000

    Update data.tf

--- data.tf
+++ data.tf
@@ -23,6 +23,7 @@ data "aws_inspector_rules_packages" "available" {}
 # # ---------------------------------------------------------------------------------------------------------------------#
 data "aws_availability_zones" "available" {
   state = "available"
+  exclude_zone_ids = ["use1-az3"]
 }
 
 data "aws_availability_zone" "all" {
