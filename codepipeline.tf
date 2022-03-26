912c891a967fbfd0a3b76190cf2c3684f8e16520
MagenX <magenx@users.noreply.github.com>
2022-03-26 13:51:05 +0000

    Update codepipeline.tf

--- codepipeline.tf
+++ codepipeline.tf
@@ -219,7 +219,7 @@ resource "aws_iam_policy" "codepipeline" {
 			"Action": [
 				"codestar-connections:UseConnection"
 			],
-			"Resource": aws_codestarconnections_connection.github.arn,
+			"Resource": "${aws_codestarconnections_connection.github.arn}",
 			"Condition": {
 				"ForAllValues:StringEquals": {
 					"codestar-connections:FullRepositoryId": var.app["source_repo"]
