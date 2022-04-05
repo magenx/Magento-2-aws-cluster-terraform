3c0243b13eb36567c97bf9375a4efc0f94d41e08
MagenX <magenx@users.noreply.github.com>
2022-04-05 13:34:33 +0100

    Update ssm.sh

--- imagebuilder/ssm.sh
+++ imagebuilder/ssm.sh
@@ -2,8 +2,10 @@
 
 AWS_DEFAULT_REGION="$(curl -s http://169.254.169.254/latest/meta-data/placement/region)"
 
+## install SSM agent to manage EC2 Instance and ImageBuilder AMI build
 wget https://s3.${AWS_DEFAULT_REGION}.amazonaws.com/amazon-ssm-${AWS_DEFAULT_REGION}/latest/debian_arm64/amazon-ssm-agent.deb
 dpkg -i amazon-ssm-agent.deb
 systemctl enable amazon-ssm-agent
 
+## trigger system cleanup
 touch perform_cleanup
