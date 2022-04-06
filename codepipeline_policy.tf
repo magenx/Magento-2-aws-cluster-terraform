b103a474c8c4f0d31a8be74698da8bb7cc92db50
MagenX <magenx@users.noreply.github.com>
2022-04-06 07:22:56 +0100

    Update codepipeline_policy.tf

--- codepipeline_policy.tf
+++ codepipeline_policy.tf
@@ -167,3 +167,89 @@ resource "aws_iam_role_policy_attachment" "codebuild" {
   policy_arn = aws_iam_policy.codebuild.arn
   role       = aws_iam_role.codebuild.id
 }
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CodePipeline role
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_role" "codepipeline" {
+  name = "${local.project}-codepipeline-role"
+  description = "Allows CodePipeline to call AWS services on your behalf."
+  path = "/service-role/"
+  assume_role_policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "codepipeline.amazonaws.com"
+          }
+          Sid = ""
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+  tags = {
+     Name = "${local.project}-codepipeline-role"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create policy for CodePipeline role
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_policy" "codepipeline" {
+  name = "${local.project}-codepipeline-policy"
+  path = "/service-role/"
+  policy = <<EOF
+{
+	"Version": "2012-10-17",
+	"Statement": [{
+			"Sid": "AllowCodeCommitActions",
+			"Effect": "Allow",
+			"Action": [
+				"codecommit:GetCommit",
+				"codecommit:GetRepository",
+				"codecommit:GetBranch"
+			],
+			"Resource": "${aws_codecommit_repository.app.arn}"
+		},
+                {
+                        "Action": [
+                                "sns:Publish"
+                        ],
+                        "Effect": "Allow",
+                        "Resource": "${aws_sns_topic.default.arn}",
+                        "Sid": "AllowSNSPublish"
+                },
+		{
+			"Sid": "AllowCodeStarConnectionActions",
+			"Effect": "Allow",
+			"Action": [
+				"codestar-connections:UseConnection"
+			],
+			"Resource": "${aws_codestarconnections_connection.github.arn}"
+		},
+		{
+			"Sid": "AllowCodeBuildActions",
+			"Effect": "Allow",
+			"Action": [
+				"codebuild:StartBuild",
+				"codebuild:StartBuildBatch",
+				"codebuild:BatchGetBuilds",
+				"codebuild:BatchGetBuildBatches"
+			],
+			"Resource": [
+				"${aws_codebuild_project.this.arn}",
+				"${aws_codebuild_project.install.arn}"
+			]
+		}
+	]
+}
+EOF
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Attach policy for CodePipeline role
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_role_policy_attachment" "codepipeline" {
+  policy_arn = aws_iam_policy.codepipeline.arn
+  role       = aws_iam_role.codepipeline.id
+}
