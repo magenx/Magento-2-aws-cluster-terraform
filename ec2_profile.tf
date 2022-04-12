af51dc1d42317655282181e3a4fec7afff6e1bd8
MagenX <magenx@users.noreply.github.com>
2022-04-12 09:24:40 +0100

    Create ec2_profile.tf

---
+++ ec2_profile.tf
@@ -0,0 +1,104 @@
+
+
+
+///////////////////////////////////////////////////////////[ EC2 PROFILE ]////////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create EC2 service role
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_role" "ec2" {
+  for_each = var.ec2
+  name = "${local.project}-EC2InstanceRole-${each.key}"
+  description = "Allows EC2 instances to call AWS services on your behalf"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Attach policies to EC2 service role
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_role_policy_attachment" "ec2" {
+  for_each = { for policy in [ for role,policy in setproduct(keys(var.ec2),var.ec2_instance_profile_policy): { role = policy[0] , policy = policy[1]} ] : "${policy.role}-${policy.policy}" => policy }
+  role       = aws_iam_role.ec2[each.value.role].name
+  policy_arn = each.value.policy
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create inline policy for EC2 service role to publish sns message
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_role_policy" "sns_publish" {
+  for_each = var.ec2
+  name = "EC2ProfileSNSPublishPolicy${title(each.key)}"
+  role = aws_iam_role.ec2[each.key].id
+
+  policy = jsonencode({
+  Version = "2012-10-17",
+  Statement = [
+    {
+      Sid    = "EC2ProfileSNSPublishPolicy${each.key}",
+      Effect = "Allow",
+      Action = [
+            "sns:Publish"
+      ],
+      Resource = aws_sns_topic.default.arn
+ }]
+})
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create inline policy for EC2 service role to limit CodeCommit access
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_role_policy" "codecommit_access" {
+  for_each = var.ec2
+  name = "${local.project}PolicyForCodeCommitAccess${title(each.key)}"
+  role = aws_iam_role.ec2[each.key].id
+
+  policy = jsonencode({
+  Version = "2012-10-17",
+  Statement = [
+    {
+      Sid    = "codecommitaccessapp${each.key}",
+      Effect = "Allow",
+      Action = [
+            "codecommit:Get*",
+            "codecommit:List*",
+            "codecommit:GitPull"
+      ],
+      Resource = aws_codecommit_repository.app.arn
+      Condition = {
+                StringEqualsIfExists = {
+                    "codecommit:References" = ["refs/heads/main"]
+    }
+   }
+},
+     {
+      Sid    = "codecommitaccessservices${each.key}", 
+      Effect = "Allow",
+      Action = [
+            "codecommit:Get*",
+            "codecommit:List*",
+            "codecommit:GitPull"
+      ],
+      Resource = aws_codecommit_repository.services.arn
+    }]
+})
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create EC2 Instance Profile
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_instance_profile" "ec2" {
+  for_each = var.ec2
+  name     = "${local.project}-EC2InstanceProfile-${each.key}"
+  role     = aws_iam_role.ec2[each.key].name
+}
+    
