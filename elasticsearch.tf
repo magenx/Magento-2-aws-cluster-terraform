5a6170d439bdda773b577d20015037e4930725ee
MagenX <magenx@users.noreply.github.com>
2022-04-12 20:30:13 +0100

    Update elasticsearch.tf

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -4,15 +4,15 @@
 //////////////////////////////////////////////////////////[ ELASTICSEARCH ]///////////////////////////////////////////////
 
 # # ---------------------------------------------------------------------------------------------------------------------#
-# Create ElasticSearch service linked role if not exists
+# Create OpenSearch service linked role if not exists
 # # ---------------------------------------------------------------------------------------------------------------------#
-resource "null_resource" "es" {
+resource "null_resource" "opensearch" {
   provisioner "local-exec" {
   interpreter = ["/bin/bash", "-c"]
   command = <<EOF
-          exit_code=$(aws iam get-role --role-name AWSServiceRoleForAmazonElasticsearchService > /dev/null 2>&1 ; echo $?)
+          exit_code=$(aws iam get-role --role-name AWSServiceRoleForAmazonOpenSearchService > /dev/null 2>&1 ; echo $?)
           if [[ $exit_code -ne 0 ]]; then
-          aws iam create-service-linked-role --aws-service-name es.amazonaws.com
+          aws iam create-service-linked-role --aws-service-name opensearchservice.amazonaws.com
           fi
 EOF
  }
@@ -20,18 +20,18 @@ EOF
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create ElasticSearch domain
 # # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_elasticsearch_domain" "this" {
-  depends_on = [null_resource.es]
-  domain_name           = "${local.project}-elk"
-  elasticsearch_version = var.elk["elasticsearch_version"]
+resource "aws_opensearch_domain" "this" {
+  depends_on = [null_resource.opensearch]
+  domain_name           = "${local.project}-opensearch"
+  engine_version = var.opensearch["engine_version"]
   cluster_config {
-    instance_type  = var.elk["instance_type"]
-    instance_count = var.elk["instance_count"]
-  zone_awareness_enabled = var.elk["instance_count"] > 1 ? true : false
+    instance_type  = var.opensearch["instance_type"]
+    instance_count = var.opensearch["instance_count"]
+  zone_awareness_enabled = var.opensearch["instance_count"] > 1 ? true : false
   dynamic "zone_awareness_config" {
-     for_each = var.elk["instance_count"] > 1 ? [var.elk["instance_count"]] : []
+     for_each = var.opensearch["instance_count"] > 1 ? [var.opensearch["instance_count"]] : []
      content {
-        availability_zone_count = var.elk["instance_count"]
+        availability_zone_count = var.opensearch["instance_count"]
       }
     }
   }
@@ -39,20 +39,20 @@ resource "aws_elasticsearch_domain" "this" {
     enabled = true
   }
   ebs_options {
-    ebs_enabled = var.elk["ebs_enabled"]
-    volume_type = var.elk["volume_type"]
-    volume_size = var.elk["volume_size"]
+    ebs_enabled = var.opensearch["ebs_enabled"]
+    volume_type = var.opensearch["volume_type"]
+    volume_size = var.opensearch["volume_size"]
   }
   vpc_options {
-    subnet_ids = slice(values(aws_subnet.this).*.id, 0, var.elk["instance_count"])
-    security_group_ids = [aws_security_group.elk.id]
+    subnet_ids = slice(values(aws_subnet.this).*.id, 0, var.opensearch["instance_count"])
+    security_group_ids = [aws_security_group.opensearch.id]
   }
   tags = {
-    Name = "${local.project}-elk"
+    Name = "${local.project}-opensearch"
   }
   log_publishing_options {
-    cloudwatch_log_group_arn = aws_cloudwatch_log_group.elk.arn
-    log_type                 = var.elk["log_type"]
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.opensearch.arn
+    log_type                 = var.opensearch["log_type"]
   }
   access_policies = <<EOF
 {
@@ -68,7 +68,7 @@ resource "aws_elasticsearch_domain" "this" {
       "Action": [
         "es:*"
       ],
-      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.project}-elk/*"
+      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.project}-opensearch/*"
     }
   ]
 }
@@ -77,12 +77,12 @@ EOF
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create CloudWatch log group for ElasticSearch log stream
 # # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_cloudwatch_log_group" "elk" {
-  name = "${local.project}-elk"
+resource "aws_cloudwatch_log_group" "opensearch" {
+  name = "${local.project}-opensearch"
 }
 
-resource "aws_cloudwatch_log_resource_policy" "elk" {
-  policy_name = "${local.project}-elk"
+resource "aws_cloudwatch_log_resource_policy" "opensearch" {
+  policy_name = "${local.project}-opensearch"
 
   policy_document = <<EOF
 {
