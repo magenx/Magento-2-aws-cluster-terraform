cf246ad454aa7333b7496b68dcaf7b979f4fab01
MagenX <magenx@users.noreply.github.com>
2022-04-13 14:55:03 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,18 +13,6 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.0"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 2.0"
-    }
-    external = {
-      source  = "hashicorp/external"
-      version = "~> 2.0"
-    }
   }
 }
 
