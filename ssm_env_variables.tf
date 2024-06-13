5ac7d3dbba2d4de9585b931d1050593d11216a58
MagenX <magenx@users.noreply.github.com>
2024-06-13 11:56:13 +0200

    Update ssm_env_variables.tf

--- ssm_env_variables.tf
+++ ssm_env_variables.tf
@@ -43,7 +43,7 @@ resource "aws_ssm_parameter" "env" {
 "SES_PASSWORD" : "${aws_iam_access_key.ses_smtp_user_access_key.ses_smtp_password_v4}",
 "SES_ENDPOINT" : "email-smtp.${data.aws_region.current.name}.amazonaws.com",
 "DATABASE_ENDPOINT" : "${aws_db_instance.this.endpoint}",
-"DATABASE_NAME" : "${aws_db_instance.this.name}",
+"DATABASE_NAME" : "${aws_db_instance.this.identifier}",
 "DATABASE_USER_NAME" : "${aws_db_instance.this.username}",
 "DATABASE_PASSWORD" : "${random_password.this["rds"].result}",
 "ADMIN_PATH" : "admin_${random_string.this["admin_path"].result}",
