37f3a21e8765c8b32e04924ea25caef2de69f9ae
MagenX <magenx@users.noreply.github.com>
2024-06-17 12:19:12 +0200

    Update imagebuilder.tf

--- imagebuilder.tf
+++ imagebuilder.tf
@@ -106,7 +106,7 @@ resource "aws_imagebuilder_infrastructure_configuration" "this" {
   name                  = "${local.project}-${each.key}-imagebuilder-infrastructure"
   description           = "ImageBuilder infrastructure for ${each.key} in ${local.project}"
   instance_profile_name = aws_iam_instance_profile.ec2[each.key].name
-  instance_types        = each.value
+  instance_types        = [each.value]
   security_group_ids    = [aws_security_group.ec2.id]
   sns_topic_arn         = aws_sns_topic.default.arn
   subnet_id             = values(aws_subnet.this).0.id
