f1a55ed2fd3a09185b4e674c51622ec7b931aa5b
MagenX <magenx@users.noreply.github.com>
2024-06-28 12:42:09 +0200

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -12,7 +12,9 @@ resource "aws_cloudfront_origin_access_identity" "this" {
 
 resource "aws_cloudfront_distribution" "this" {
   enabled             = true
+  retain_on_delete    = false
   is_ipv6_enabled     = true
+  http_version        = http2and3
   web_acl_id          = aws_wafv2_web_acl.this.arn
   price_class         = "PriceClass_100"
   comment             = "${var.app["domain"]} assets"
@@ -32,41 +34,46 @@ resource "aws_cloudfront_distribution" "this" {
   }
   
   default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "${var.app["domain"]}-media-assets"
 
-    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.s3.id
-    cache_policy_id          = data.aws_cloudfront_cache_policy.s3.id
-
     viewer_protocol_policy = "https-only"
 
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.media.id
+    cache_policy_id          = data.aws_cloudfront_cache_policy.media.id
   }
   
   origin {
-	domain_name = var.app["domain"]
-	origin_id   = "${var.app["domain"]}-static-assets"
+    domain_name = aws_elb.this["external"].dns_name
+    origin_id   = "${var.app["domain"]}-static-assets"
 
-	custom_origin_config {
-		http_port              = 80
-		https_port             = 443
-		origin_protocol_policy = "https-only"
-		origin_ssl_protocols   = ["TLSv1.2"]
-	}
-  }
+    custom_origin_config {
+       http_port              = 80
+       https_port             = 443
+       origin_protocol_policy = "https-only"
+       origin_ssl_protocols   = ["TLSv1.2"]
+   }
+
+    custom_header {
+      name  = "X-Magenx-Header"
+      value = random_uuid.this.result
+   }
+ }
 
   ordered_cache_behavior {
-	path_pattern     = "/static/*"
-	allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-	cached_methods   = ["GET", "HEAD"]
-	target_origin_id = "${var.app["domain"]}-static-assets"
+       path_pattern     = "/static/*"
+       allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+       cached_methods   = ["GET", "HEAD"]
+       target_origin_id = "${var.app["domain"]}-static-assets"
 	
-    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.custom.id
-    cache_policy_id          = data.aws_cloudfront_cache_policy.custom.id
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.static.id
+    cache_policy_id          = data.aws_cloudfront_cache_policy.static.id
 
     viewer_protocol_policy = "https-only"
     compress               = true
 }
+
   logging_config {
     include_cookies = false
     bucket          = aws_s3_bucket.this["system"].bucket_domain_name
