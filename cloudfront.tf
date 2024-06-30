917eb1e8b4835ced88712129db35bacd5bc08242
MagenX <magenx@users.noreply.github.com>
2024-06-30 17:05:13 +0200

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -89,7 +89,6 @@ resource "aws_cloudfront_distribution" "this" {
 
   viewer_certificate {
     cloudfront_default_certificate = true
-    minimum_protocol_version = "TLSv1.2_2021"
   }
   
   tags = {
