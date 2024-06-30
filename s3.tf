88ce61518e7b90fc32b9cc4cca10394f34d575eb
MagenX <magenx@users.noreply.github.com>
2024-06-30 12:53:52 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -8,10 +8,10 @@
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_s3_bucket" "this" {
   for_each      = var.s3
-  bucket        = "${local.project}-${random_string.s3[each.key].id}-${each.key}"
+  bucket        = "${local.project}-${each.key}"
   force_destroy = true
   tags = {
-    Name        = "${local.project}-${random_string.s3[each.key].id}-${each.key}"
+    Name        = "${local.project}-${each.key}"
   }
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
@@ -37,8 +37,8 @@ resource "aws_s3_bucket_versioning" "this" {
 # Create S3 bucket encryption
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  for_each = var.s3
-  bucket   = "${local.project}-${random_string.s3[each.key].id}-${each.key}"
+  for_each = aws_s3_bucket.this
+  bucket   = aws_s3_bucket.this[each.key].id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -50,7 +50,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 # # ---------------------------------------------------------------------------------------------------------------------#	  
 resource "aws_s3_bucket_public_access_block" "this" {
   for_each = {for name in var.s3: name => name if name != "media"}
-  bucket = "${local.project}-${random_string.s3[each.key].id}-${each.key}"  
+  bucket = aws_s3_bucket.this[each.key].id  
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
@@ -59,147 +59,160 @@ resource "aws_s3_bucket_public_access_block" "this" {
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create policy for CloudFront to limit S3 media bucket access
 # # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_s3_bucket_policy_document" "media" {
+  statement {
+    sid       = "AllowCloudFrontAccess"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = [
+      "${aws_s3_bucket.this["media"].arn}/*.jpg",
+      "${aws_s3_bucket.this["media"].arn}/*.jpeg",
+      "${aws_s3_bucket.this["media"].arn}/*.png",
+      "${aws_s3_bucket.this["media"].arn}/*.gif",
+      "${aws_s3_bucket.this["media"].arn}/*.webp"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.this.iam_arn]
+    }
+  }
+
+  statement {
+    sid       = "AllowEC2PutObject"
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = [
+      "${aws_s3_bucket.this["media"].arn}",
+      "${aws_s3_bucket.this["media"].arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = values(aws_iam_instance_profile.ec2)[*].arn
+    }
+    conditions {
+      test     = "StringEquals"
+      variable = "aws:SourceVpc"
+      values   = [aws_vpc.this.id]
+    }
+  }
+
+  statement {
+    sid       = "AllowEC2GetObject"
+    effect    = "Allow"
+    actions   = ["s3:GetObject", "s3:GetObjectAcl"]
+    resources = [
+      "${aws_s3_bucket.this["media"].arn}",
+      "${aws_s3_bucket.this["media"].arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = values(aws_iam_instance_profile.ec2)[*].arn
+    }
+  }
+
+  statement {
+    sid       = "AllowEC2ListBucket"
+    effect    = "Allow"
+    actions   = ["s3:GetBucketLocation", "s3:ListBucket"]
+    resources = "${aws_s3_bucket.this["media"].arn}"
+    principals {
+      type        = "AWS"
+      identifiers = values(aws_iam_instance_profile.ec2)[*].arn
+    }
+  }
+}
+
 resource "aws_s3_bucket_policy" "media" {
-   bucket = aws_s3_bucket.this["media"].id
-   policy = jsonencode({
-   Id = "PolicyForMediaStorageAccess"
-   Statement = [
-	  {
-         Action = "s3:GetObject"
-         Effect = "Allow"
-         Principal = {
-            AWS = aws_cloudfront_origin_access_identity.this.iam_arn
-         }
-         Resource = [
-            "${aws_s3_bucket.this["media"].arn}/*.jpg",
-            "${aws_s3_bucket.this["media"].arn}/*.jpeg",
-            "${aws_s3_bucket.this["media"].arn}/*.png",
-            "${aws_s3_bucket.this["media"].arn}/*.gif",
-            "${aws_s3_bucket.this["media"].arn}/*.webp"
-         ]
-      }, 
-      {
-         Action = ["s3:PutObject"],
-         Effect = "Allow"
-         Principal = {
-            AWS = values(aws_iam_instance_profile.ec2)[*].arn
-         }
-         Resource = [
-            "${aws_s3_bucket.this["media"].arn}",
-            "${aws_s3_bucket.this["media"].arn}/*"
-         ]
-         Condition = {
-            StringEquals = {
-                "aws:SourceVpc" = [ aws_vpc.this.id ]
-         }
-	}
-      }, 
-      {
-         Action = ["s3:GetObject", "s3:GetObjectAcl"],
-         Effect = "Allow"
-         Principal = {
-            AWS = values(aws_iam_instance_profile.ec2)[*].arn
-         }
-         Resource = [
-            "${aws_s3_bucket.this["media"].arn}",
-            "${aws_s3_bucket.this["media"].arn}/*"
-         ]
-      }, 
-      {
-         Action = ["s3:GetBucketLocation", "s3:ListBucket"],
-         Effect = "Allow"
-         Principal = {
-            AWS = values(aws_iam_instance_profile.ec2)[*].arn
-         }
-         Resource = "${aws_s3_bucket.this["media"].arn}"
-      }, 
-	  ] 
-	  Version = "2012-10-17"
-   })
+  bucket = aws_s3_bucket.this["media"].id
+  policy = data.aws_iam_policy_document.media.json
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create S3 bucket policy for ALB to write access logs
 # # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_policy_document" "system" {
+  statement {
+    sid    = "ALBWriteLogs"
+    effect = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = ["${aws_s3_bucket.this["system"].arn}/ALB/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_elb_service_account.current.arn]
+    }
+  }
+
+  statement {
+    sid    = "AllowS3Access"
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.this["system"].arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [
+        aws_iam_role.codebuild.arn,
+        aws_iam_role.codepipeline.arn,
+        aws_iam_role.config.arn
+      ]
+    }
+  }
+
+  statement {
+    sid    = "CloudFrontAccess"
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.this["system"].arn}/CloudFront/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    conditions {
+      test     = "StringEquals"
+      variable = "AWS:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+    conditions {
+      test     = "ArnLike"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/*"]
+    }
+  }
+  version = "2012-10-17"
+}
+
 resource "aws_s3_bucket_policy" "system" {
   bucket = aws_s3_bucket.this["system"].id
-  policy = jsonencode(
-            {
-  Id = "PolicyALBWriteLogs"
-  Version = "2012-10-17"
-  Statement = [
-    {
-      Action = [
-        "s3:PutObject"
-      ],
-      Effect = "Allow"
-      Resource = "${aws_s3_bucket.this["system"].arn}/ALB/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
-      Principal = {
-        AWS = [
-          data.aws_elb_service_account.current.arn
-        ]
-      }
-    },
-    {
-      Action = [
-        "s3:PutObject",
-        "s3:GetObject"
-      ],
-      Effect = "Allow"
-      Resource = "${aws_s3_bucket.this["system"].arn}/*"
-      Principal = {
-        AWS = [
-          aws_iam_role.codebuild.arn,
-          aws_iam_role.codepipeline.arn,
-          aws_iam_role.config.arn
-        ]
-     }
-  },
- {
-  Action = [
-            "s3:PutObject"
-          ],
-          Effect   = "Allow"
-          Resource = "${aws_s3_bucket.this["system"].arn}/CloudFront/*"
-          Principal = {
-            Service = "cloudfront.amazonaws.com"
-          },
-          Condition = {
-            StringEquals = {
-              "AWS:SourceAccount" = "${data.aws_caller_identity.current.account_id}"
-            },
-            ArnLike = {
-              "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/*"
-            }
-          }
-}
-]
-})
+  policy = data.aws_iam_policy_document.system.json
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create S3 bucket policy for CodePipeline access
 # # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_iam_policy_document" "backup" {
+  statement {
+    actions   = ["s3:PutObject"]
+    effect    = "Allow"
+    resources = [aws_s3_bucket.this["backup"].arn]
+    principals {
+      type        = "AWS"
+      identifiers = [
+        aws_iam_role.codebuild.arn,
+        aws_iam_role.codepipeline.arn,
+        aws_iam_role.codedeploy.arn
+      ]
+    }
+  }
+  version = "2012-10-17"
+}
+
 resource "aws_s3_bucket_policy" "backup" {
   bucket = aws_s3_bucket.this["backup"].id
-  policy = jsonencode({
-  Id = "PolicyForBackupBucket"
-  Version = "2012-10-17"
-  Statement = [
-    {
-      Action = [
-        "s3:PutObject"
-      ],
-      Effect = "Allow"
-      Resource = "${aws_s3_bucket.this["backup"].arn}/*"
-      Principal = {
-        AWS = [
-          aws_iam_role.codebuild.arn,
-          aws_iam_role.codepipeline.arn,
-          aws_iam_role.codedeploy.arn
-        ]
-      }
-    }
-  ]
-})
+  policy = data.aws_iam_policy_document.backup.json
 }
 
 
