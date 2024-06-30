cbb555940f0e98bf82b51f454222b8d4060f331f
MagenX <magenx@users.noreply.github.com>
2024-06-30 13:39:13 +0200

    Update eventbridge.tf

--- eventbridge.tf
+++ eventbridge.tf
@@ -6,48 +6,34 @@
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create EventBridge service role
 # # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_iam_policy_document" "eventbridge_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "eventbridge_service_role" {
   name = "${local.project}-EventBridgeServiceRole"
   description = "Provides EventBridge manage events on your behalf."
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "events.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
+  assume_role_policy = data.aws_iam_policy_document.eventbridge_assume_role.json
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create policy for EventBridge role to start CodePipeline
 # # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_iam_policy" "eventbridge_service_role" {
-  name = "${local.project}-start-codepipeline"
-  path = "/service-role/"
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "codepipeline:StartPipelineExecution"
-            ],
-            "Resource": [
-                "${aws_codepipeline.this.arn}"
-            ]
-        }
-    ]
+
+data "aws_iam_policy_document" "eventbridge_service_role" {
+  statement {
+    sid     = "eventspolicy"
+    actions = ["codepipeline:StartPipelineExecution"]
+    resources = ["${aws_codepipeline.this.arn}"]
+  }
 }
-EOF
-}
+
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Attach policies to EventBridge role
 # # ---------------------------------------------------------------------------------------------------------------------#
