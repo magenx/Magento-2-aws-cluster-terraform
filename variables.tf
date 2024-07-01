eaa4b01f04256e43e1f65d10c9880a38f7e013c0
MagenX <magenx@users.noreply.github.com>
2024-07-01 13:04:52 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,10 +4,6 @@ locals {
    # Create global project name to be assigned to all resources
    project = lower("${var.app["brand"]}-${random_string.this["project"].result}")
    environment = lower(terraform.workspace)
-   validation {
-     condition     = terraform.workspace == "default"
-     error_message = "Error: Default workspace is not allowed. Please switch to a non-default workspace."
-   }
 }
 
 variable "password" {
