838c491d1f6b152bd27f7e0ba27666bac5a36fd5
MagenX <magenx@users.noreply.github.com>
2024-07-13 15:09:26 +0200

    Update build.sh

--- packer/build.sh
+++ packer/build.sh
@@ -9,18 +9,11 @@ AWSTOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-m
 INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: ${AWSTOKEN}" http://169.254.169.254/latest/meta-data/instance-id)
 INSTANCE_TYPE=$(curl -s -H "X-aws-ec2-metadata-token: ${AWSTOKEN}" http://169.254.169.254/latest/meta-data/instance-type)
 
-# remove old aws cli v1
-sudo apt-get -y remove awscli
+# get parameters
 sudo apt-get update
-sudo apt-get -qqy install jq unzip
+sudo apt-get -qqy install jq
 
-# get latest aws cli v2
-cd /tmp
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
-sudo unzip awscliv2.zip -d /root/
-sudo rm awscliv2.zip
-sudo /root/aws/install --bin-dir /usr/bin --install-dir /root/aws --update
-
+sudo sh -c "echo 'export PARAMETERSTORE_NAME=${PARAMETERSTORE_NAME}' >> /root/.bashrc"
 PARAMETER=$(sudo aws ssm get-parameter --name "${PARAMETERSTORE_NAME}" --query 'Parameter.Value' --output text)
 declare -A parameter
 while IFS== read -r key value; do parameter["$key"]="$value"; done < <(echo ${PARAMETER} | jq -r 'to_entries[] | .key + "=" + .value')
@@ -28,6 +21,12 @@ while IFS== read -r key value; do parameter["$key"]="$value"; done < <(echo ${PA
 ## installation
 sudo apt-get -qqy install ${parameter["LINUX_PACKAGES"]}
 sudo pip3 install git-remote-codecommit
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo sh
+. "$HOME/.cargo/env"
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Frontend and admin instance configuration
+# # ---------------------------------------------------------------------------------------------------------------------#
 
 ## create user
 sudo useradd -d /home/${parameter["BRAND"]} -s /sbin/nologin ${parameter["BRAND"]}
@@ -43,15 +42,49 @@ sudo chown -R ${parameter["BRAND"]}:${parameter["BRAND"]} /home/${parameter["BRA
 sudo chmod 2750 ${parameter["WEB_ROOT_PATH"]} /home/${parameter["BRAND"]}/{.config,.cache,.local,.composer}
 sudo setfacl -R -m m:rx,u:${parameter["BRAND"]}:rwX,g:${parameter["PHP_USER"]}:r-X,o::-,d:u:${parameter["BRAND"]}:rwX,d:g:${parameter["PHP_USER"]}:r-X,d:o::- ${parameter["WEB_ROOT_PATH"]}
 
-sudo sh -c "echo '${parameter["EFS_DNS_TARGET"]}:/data/var ${parameter["WEB_ROOT_PATH"]}/var nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev 0 0' >> /etc/fstab"
-sudo sh -c "echo '${parameter["EFS_DNS_TARGET"]}:/data/pub/media ${parameter["WEB_ROOT_PATH"]}/pub/media nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev 0 0' >> /etc/fstab"
+sudo sh -c "cat > /home/${parameter["BRAND"]}/.env <<END
+MODE="production"
+DOMAIN="${parameter["BRAND"]}"
+ADMIN_PATH="${parameter["BRAND"]}"
+EXTERNAL_ALB_DNS_NAME="${parameter["EXTERNAL_ALB_DNS_NAME"]}"
+INTERNAL_ALB_DNS_NAME="${parameter["INTERNAL_ALB_DNS_NAME"]}"
+SES_ENDPOINT="${parameter["SES_ENDPOINT"]}"
+REDIS_CACHE_BACKEND="${parameter["REDIS_CACHE_BACKEND"]}"
+REDIS_SESSION_BACKEND="${parameter["REDIS_SESSION_BACKEND"]}"
+REDIS_CACHE_BACKEND_RO="${parameter["REDIS_CACHE_BACKEND_RO"]}"
+REDIS_SESSION_BACKEND_RO="${parameter["REDIS_SESSION_BACKEND_RO"]}"
+REDIS_PASSWORD="${parameter["REDIS_PASSWORD"]}"
+RABBITMQ_ENDPOINT="${parameter["RABBITMQ_ENDPOINT"]}"
+RABBITMQ_PASSWORD="${parameter["RABBITMQ_PASSWORD"]}"
+CRYPT_KEY="${parameter["CRYPT_KEY"]}"
+GRAPHQL_ID_SALT="${parameter["GRAPHQL_ID_SALT"]}"
+DATABASE_ENDPOINT="${parameter["DATABASE_ENDPOINT"]}"
+DATABASE_NAME="${parameter["DATABASE_NAME"]}"
+DATABASE_USER="${parameter["DATABASE_USER"]}"
+DATABASE_PASSWORD="${parameter["DATABASE_PASSWORD"]}"
+OPENSEARCH_ENDPOINT="${parameter["OPENSEARCH_ENDPOINT"]}"
+OPENSEARCH_ADMIN="${parameter["OPENSEARCH_ADMIN"]}"
+INDEXER_PASSWORD="${parameter["INDEXER_PASSWORD"]}"
+ENV_DATE="$(date -u "+%a, %d %b %Y %H:%M:%S %z")"
+END
+"
+
+cd /tmp
+sudo git clone https://github.com/aws/efs-utils
+cd efs-utils
+sudo ./build-deb.sh
+sudo apt-get -y install ./build/amazon-efs-utils*deb
+sudo rm -rf ~/.cargo ~/.rustup
+
+sudo sh -c "echo '${parameter["EFS_SYSTEM_ID"]}:/ ${parameter["WEB_ROOT_PATH"]}/var efs _netdev,noresvport,tls,iam,accesspoint=${parameter["EFS_ACCESS_POINT_VAR"]} 0 0' >> /etc/fstab"
+sudo sh -c "echo '${parameter["EFS_SYSTEM_ID"]}:/ ${parameter["WEB_ROOT_PATH"]}/pub/media efs _netdev,noresvport,tls,iam,accesspoint=${parameter["EFS_ACCESS_POINT_MEDIA"]} 0 0' >> /etc/fstab"
 
 sudo mkdir -p ${parameter["WEB_ROOT_PATH"]}/{pub/media,var}
 sudo chown -R ${parameter["BRAND"]}:${parameter["PHP_USER"]} ${parameter["WEB_ROOT_PATH"]}/
 sudo chmod 2770 ${parameter["WEB_ROOT_PATH"]}/{pub/media,var}
 
 ## install nginx
-curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+curl https://nginx.org/keys/nginx_signing.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" > /etc/apt/sources.list.d/nginx.list'
 
 ## install php + phpmyadmin
@@ -105,9 +138,83 @@ END
 "
 
 
-sudo sh -c "cat > ${parameter["PHP_OPCACHE_INI"]} <<END
-zend_extension=opcache.so
-opcache.enable = 1
+sudo sh -c "cat > ${parameter["PHP_FPM_POOL"]} <<END
+[${parameter["BRAND"]}]
+
+;;
+;; Pool user
+user = php-${parameter["BRAND"]}
+group = php-${parameter["BRAND"]}
+
+listen = /var/run/${parameter["BRAND"]}.sock
+listen.owner = nginx
+listen.group = php-${parameter["BRAND"]}
+listen.mode = 0660
+
+;;
+;; Pool size and settings
+pm = ondemand
+pm.max_children = 100
+pm.start_servers = 2
+pm.min_spare_servers = 1
+pm.max_spare_servers = 3
+pm.max_requests = 10000
+
+;;
+;; [php ini] settings
+php_admin_flag[expose_php] = Off
+php_admin_flag[short_open_tag] = On
+php_admin_flag[display_errors] = Off
+php_admin_flag[log_errors] = On
+php_admin_flag[mysql.allow_persistent] = On
+php_admin_flag[mysqli.allow_persistent] = On
+php_admin_value[default_charset] = "UTF-8"
+php_admin_value[memory_limit] = 1024M
+php_admin_value[max_execution_time] = 7200
+php_admin_value[max_input_time] = 7200
+php_admin_value[max_input_vars] = 50000
+php_admin_value[post_max_size] = 64M
+php_admin_value[upload_max_filesize] = 64M
+php_admin_value[realpath_cache_size] = 4096k
+php_admin_value[realpath_cache_ttl] = 86400
+php_admin_value[session.gc_maxlifetime] = 28800
+php_admin_value[error_log] = "${parameter["WEB_ROOT_PATH"]}/var/log/php-fpm-error.log"
+php_admin_value[date.timezone] = "${parameter["TIMEZONE"]}"
+php_admin_value[upload_tmp_dir] = "${parameter["WEB_ROOT_PATH"]}/var/tmp"
+php_admin_value[sys_temp_dir] = "${parameter["WEB_ROOT_PATH"]}/var/tmp"
+
+;;
+;; [opcache] settings
+php_admin_flag[opcache.enable] = On
+php_admin_flag[opcache.use_cwd] = On
+php_admin_flag[opcache.validate_root] = On
+php_admin_flag[opcache.revalidate_path] = Off
+php_admin_flag[opcache.validate_timestamps] = Off
+php_admin_flag[opcache.save_comments] = On
+php_admin_flag[opcache.load_comments] = On
+php_admin_flag[opcache.fast_shutdown] = On
+php_admin_flag[opcache.enable_file_override] = Off
+php_admin_flag[opcache.inherited_hack] = On
+php_admin_flag[opcache.consistency_checks] = Off
+php_admin_flag[opcache.protect_memory] = Off
+php_admin_value[opcache.memory_consumption] = 512
+php_admin_value[opcache.interned_strings_buffer] = 4
+php_admin_value[opcache.max_accelerated_files] = 60000
+php_admin_value[opcache.max_wasted_percentage] = 5
+php_admin_value[opcache.file_update_protection] = 2
+php_admin_value[opcache.optimization_level] = 0xffffffff
+php_admin_value[opcache.blacklist_filename] = "/etc/php/${parameter["PHP_VERSION"]}/fpm/conf.d/opcache.blacklist"
+php_admin_value[opcache.max_file_size] = 0
+php_admin_value[opcache.force_restart_timeout] = 60
+php_admin_value[opcache.error_log] = "${parameter["WEB_ROOT_PATH"]}/var/log/opcache.log"
+php_admin_value[opcache.log_verbosity_level] = 1
+php_admin_value[opcache.preferred_memory_model] = ""
+php_admin_value[opcache.jit_buffer_size] = 536870912
+php_admin_value[opcache.jit] = 1235
+END
+"
+
+sudo sh -c "cat > /etc/php/${parameter["PHP_VERSION"]}/cli/conf.d/zz-${parameter["BRAND"]}-overrides.ini <<END
 opcache.enable_cli = 1
 opcache.memory_consumption = 512
 opcache.interned_strings_buffer = 4
@@ -126,6 +233,7 @@ opcache.fast_shutdown = 1
 opcache.enable_file_override = 0
 opcache.optimization_level = 0xffffffff
 opcache.inherited_hack = 1
+opcache.blacklist_filename=/etc/php/${parameter["PHP_VERSION"]}/cli/conf.d/opcache.blacklist
 opcache.max_file_size = 0
 opcache.consistency_checks = 0
 opcache.force_restart_timeout = 60
@@ -134,65 +242,23 @@ opcache.log_verbosity_level = 1
 opcache.preferred_memory_model = ""
 opcache.protect_memory = 0
 ;opcache.mmap_base = ""
+
+max_execution_time = 7200
+max_input_time = 7200
+memory_limit = 2048M
+post_max_size = 64M
+upload_max_filesize = 64M
+expose_php = Off
+realpath_cache_size = 4096k
+realpath_cache_ttl = 86400
+short_open_tag = On
+max_input_vars = 50000
+session.gc_maxlifetime = 28800
+mysql.allow_persistent = On
+mysqli.allow_persistent = On
+date.timezone = "${parameter["TIMEZONE"]}"
 END
 "
-
-sudo sh -c "cp ${parameter["PHP_INI"]} ${parameter["PHP_INI"]}.BACK"
-sudo sed -i 's/^\(max_execution_time = \)[0-9]*/\17200/' ${parameter["PHP_INI"]}
-sudo sed -i 's/^\(max_input_time = \)[0-9]*/\17200/' ${parameter["PHP_INI"]}
-sudo sed -i 's/^\(memory_limit = \)[0-9]*M/\12048M/' ${parameter["PHP_INI"]}
-sudo sed -i 's/^\(post_max_size = \)[0-9]*M/\164M/' ${parameter["PHP_INI"]}
-sudo sed -i 's/^\(upload_max_filesize = \)[0-9]*M/\132M/' ${parameter["PHP_INI"]}
-sudo sed -i 's/expose_php = On/expose_php = Off/' ${parameter["PHP_INI"]}
-sudo sed -i 's/;realpath_cache_size =.*/realpath_cache_size = 5M/' ${parameter["PHP_INI"]}
-sudo sed -i 's/;realpath_cache_ttl =.*/realpath_cache_ttl = 86400/' ${parameter["PHP_INI"]}
-sudo sed -i 's/short_open_tag = Off/short_open_tag = On/' ${parameter["PHP_INI"]}
-sudo sed -i 's/;max_input_vars =.*/max_input_vars = 50000/' ${parameter["PHP_INI"]}
-sudo sed -i 's/session.gc_maxlifetime = 1440/session.gc_maxlifetime = 28800/' ${parameter["PHP_INI"]}
-sudo sed -i 's/mysql.allow_persistent = On/mysql.allow_persistent = Off/' ${parameter["PHP_INI"]}
-sudo sed -i 's/mysqli.allow_persistent = On/mysqli.allow_persistent = Off/' ${parameter["PHP_INI"]}
-sudo sed -i 's/pm = dynamic/pm = ondemand/' ${parameter["PHP_FPM_POOL"]}
-sudo sed -i 's/;pm.max_requests = 500/pm.max_requests = 10000/' ${parameter["PHP_FPM_POOL"]}
-sudo sed -i 's/^\(pm.max_children = \)[0-9]*/\1100/' ${parameter["PHP_FPM_POOL"]}
-
-sudo sed -i "s/\[www\]/\[${parameter["BRAND"]}\]/" ${parameter["PHP_FPM_POOL"]}
-sudo sed -i "s/^user =.*/user = ${parameter["PHP_USER"]}/" ${parameter["PHP_FPM_POOL"]}
-sudo sed -i "s/^group =.*/group = ${parameter["PHP_USER"]}/" ${parameter["PHP_FPM_POOL"]}
-sudo sed -ri "s/;?listen.owner =.*/listen.owner = ${parameter["BRAND"]}/" ${parameter["PHP_FPM_POOL"]}
-sudo sed -ri "s/;?listen.group =.*/listen.group = ${parameter["PHP_USER"]}/" ${parameter["PHP_FPM_POOL"]}
-sudo sed -ri "s/;?listen.mode = 0660/listen.mode = 0660/" ${parameter["PHP_FPM_POOL"]}
-sudo sed -ri "s/;?listen.allowed_clients =.*/listen.allowed_clients = 127.0.0.1/" ${parameter["PHP_FPM_POOL"]}
-sudo sed -i '/sendmail_path/,$d' ${parameter["PHP_FPM_POOL"]}
-sudo sed -i '/PHPSESSID/d' ${parameter["PHP_INI"]}
-sudo sed -i "s,.*date.timezone.*,date.timezone = ${parameter["TIMEZONE"]}," ${parameter["PHP_INI"]}
-
-sudo sh -c 'cat >> ${parameter["PHP_FPM_POOL"]} <<END
-;;
-;; Custom pool settings
-php_flag[display_errors] = off
-php_admin_flag[log_errors] = on
-php_admin_value[error_log] = "${parameter["WEB_ROOT_PATH"]}/var/log/php-fpm-error.log"
-php_admin_value[default_charset] = UTF-8
-php_admin_value[memory_limit] = 2048M
-php_admin_value[date.timezone] = ${parameter["TIMEZONE"]}
-END
-'
-
-if [ "${parameter["FASTLY"]}" == "disabled" ] && [ "${INSTANCE_NAME}" != "admin" ]; then
-
-sudo apt-get -qqy install varnish
-
-cd /etc/varnish
-
-sudo git init
-sudo git remote add origin ${parameter["CODECOMMIT_SERVICES_REPO"]}
-sudo git fetch
-sudo git reset --hard origin/varnish
-sudo git checkout -t origin/varnish
-
-cp varnish.service /etc/systemd/system/
-
-fi
 
 cd /etc/nginx
 sudo git init
@@ -256,10 +322,8 @@ sudo wget https://s3.${parameter["AWS_DEFAULT_REGION"]}.amazonaws.com/amazonclou
 sudo dpkg -i amazon-cloudwatch-agent.deb
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:amazon-cloudwatch-agent-${INSTANCE_NAME}.json
 
-sudo chmod 750 /usr/bin/aws /root/aws
-apt-get install -y aptitude
-apt-get purge $(aptitude search '~i!~M!~prequired!~pimportant!~R~prequired!~R~R~prequired!~R~pimportant!~R~R~pimportant!busybox!grub!initramfs-tools' | awk '{print $2}')
 sudo apt-get remove --purge -y \
+    awscli* \
     apache2* \
     bind9* \
     samba* \
