102271e298bf860801968a296cc27ec265992b25
MagenX <magenx@users.noreply.github.com>
2024-07-15 14:27:08 +0200

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -7,15 +7,15 @@
 # Create ssl certificate for domain and subdomains
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_acm_certificate" "default" {
-  domain_name               = "${var.app["domain"]}"
-  subject_alternative_names = ["*.${var.app["domain"]}"]
+  domain_name               = "${var.magento["domain"]}"
+  subject_alternative_names = ["*.${var.magento["domain"]}"]
   validation_method         = "EMAIL"
 
   lifecycle {
     create_before_destroy   = true
   }
   tags = {
-    Name = "${local.project}-${var.app["domain"]}-cert"
+    Name = "${local.project}-${var.magento["domain"]}-cert"
   }
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
