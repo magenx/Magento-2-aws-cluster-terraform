d60fa2cbfb776641f4d963ac1ec90baec85e2e98
MagenX <magenx@users.noreply.github.com>
2024-07-15 14:06:47 +0200

    Update codepipeline.tf

--- codepipeline.tf
+++ codepipeline.tf
@@ -2,6 +2,90 @@
 
 
 ////////////////////////////////////////////////////////[ CODEPIPELINE ]//////////////////////////////////////////////////
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CodeDeploy role
+# # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_iam_policy_document" "codedeploy_assume_role" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codedeploy.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "codedeploy" {
+  name        = "${local.project}-codedeploy-role"
+  description = "Allows CodeDeploy to call AWS services on your behalf."
+  assume_role_policy = data.aws_iam_policy_document.codedeploy_assume_role.json
+  tags = {
+    Name = "${local.project}-codedeploy-role"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create policy for CodeDeploy role
+# # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_iam_policy_document" "codedeploy" {
+  statement {
+    sid       = "AllowCodeDeploySNSAlertTrigger"
+    effect    = "Allow"
+    actions   = ["sns:Publish"]
+    resources = [aws_sns_topic.default.arn]
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Attach policy for CodeDeploy role
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_role_policy" "codedeploy" {
+  role   = aws_iam_role.codedeploy.name
+  policy = data.aws_iam_policy_document.codedeploy.json
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create policy for CodeBuild role
+# # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_iam_policy_document" "codebuild_assume_role" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role" "codebuild" {
+  name        = "${local.project}-codebuild-role"
+  description = "Allows CodeBuild to call AWS services on your behalf."
+  assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role.json
+  tags = {
+    Name = "${local.project}-codebuild-role"
+  }
+}
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CodePipeline role
+# # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_iam_policy_document" "codepipeline_assume_role" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "codepipeline" {
+  name        = "${local.project}-codepipeline-role"
+  description = "Allows CodePipeline to call AWS services on your behalf."
+  assume_role_policy = data.aws_iam_policy_document.codepipeline_assume_role.json
+  tags = {
+    Name = "${local.project}-codepipeline-role"
+  }
+}
 
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create SSM Document runShellScript to pull main branch from CodeCommit
