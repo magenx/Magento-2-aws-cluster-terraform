18aecaa8be108f9d2f64172bfc0c7ba438bbcd5d
MagenX <magenx@users.noreply.github.com>
2024-07-15 13:59:29 +0200

    Update eventbridge.tf

--- eventbridge.tf
+++ eventbridge.tf
@@ -1,62 +1,24 @@
 
 
 
-////////////////////////////////////////////////////////[ EVENTBRIDGE RULES ]/////////////////////////////////////////////
+////////////////////////////////////////////////////////[ EVENTBRIDGE ROLE ]//////////////////////////////////////////////
 
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create EventBridge service role
 # # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_iam_policy_document" "eventbridge_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "eventbridge_service_role" {
   name = "${local.project}-EventBridgeServiceRole"
   description = "Provides EventBridge manage events on your behalf."
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "events.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Create policy for EventBridge role to start CodePipeline
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_iam_policy" "eventbridge_service_role" {
-  name = "${local.project}-start-codepipeline"
-  path = "/service-role/"
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "codepipeline:StartPipelineExecution"
-            ],
-            "Resource": [
-                "${aws_codepipeline.this.arn}"
-            ]
-        }
-    ]
-}
-EOF
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Attach policies to EventBridge role
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_iam_role_policy_attachment" "eventbridge" {
-  policy_arn = aws_iam_policy.eventbridge_service_role.arn
-  role       = aws_iam_role.eventbridge_service_role.name
-}
-resource "aws_iam_role_policy_attachment" "eventbridge_service_role" {
-  for_each   = var.eventbridge_policy
-  role       = aws_iam_role.eventbridge_service_role.name
-  policy_arn = each.value
+  assume_role_policy = data.aws_iam_policy_document.eventbridge_assume_role.json
 }
