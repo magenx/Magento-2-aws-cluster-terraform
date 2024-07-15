0304f359d908984bb3888a2adfb40fa353e52843
MagenX <magenx@users.noreply.github.com>
2024-07-15 14:31:14 +0200

    Update user_data.tf

--- user_data.tf
+++ user_data.tf
@@ -47,13 +47,13 @@ frontend_user_data = <<EOF
           AWSTOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
           INSTANCE_LOCAL_IP=$(curl -s -H "X-aws-ec2-metadata-token: $${AWSTOKEN}" http://169.254.169.254/latest/meta-data/local-ipv4)
         - |-
-          cd /home/${var.app["brand"]}/public_html/
-          su ${var.app["brand"]} -s /bin/bash -c "git init -b ${local.environment}"
-          su ${var.app["brand"]} -s /bin/bash -c "git remote add origin codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.app.repository_name}"
-          su ${var.app["brand"]} -s /bin/bash -c "git fetch origin ${local.environment}"
-          su ${var.app["brand"]} -s /bin/bash -c "git reset origin/${local.environment} --hard"
+          cd /home/${var.magento["brand"]}/public_html/
+          su ${var.magento["brand"]} -s /bin/bash -c "git init -b ${local.environment}"
+          su ${var.magento["brand"]} -s /bin/bash -c "git remote add origin codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.magento.repository_name}"
+          su ${var.magento["brand"]} -s /bin/bash -c "git fetch origin ${local.environment}"
+          su ${var.magento["brand"]} -s /bin/bash -c "git reset origin/${local.environment} --hard"
         - |-
           sed -i "s/INSTANCE_LOCAL_IP/$${INSTANCE_LOCAL_IP}/" /etc/nginx/sites-available/magento.conf
-          systemctl restart nginx php-fpm${var.app["php_version"]}
+          systemctl restart nginx php${var.magento["php_version"]}-fpm
 EOF
 }
