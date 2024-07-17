7e21ad58ad9d189fc6d099671f0e2c623ed45646
MagenX <magenx@users.noreply.github.com>
2024-07-17 20:41:27 +0200

    Update eventbridge.tf

--- eventbridge.tf
+++ eventbridge.tf
@@ -22,32 +22,3 @@ resource "aws_iam_role" "eventbridge_service_role" {
   description = "Provides EventBridge manage events on your behalf."
   assume_role_policy = data.aws_iam_policy_document.eventbridge_assume_role.json
 }
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Create policy for EventBridge role to start CodePipeline
-# # ---------------------------------------------------------------------------------------------------------------------#
-data "aws_iam_policy_document" "eventbridge_start_pipeline" {
-  statement {
-    sid     = "eventspolicy"
-    actions = ["codepipeline:StartPipelineExecution"]
-    resources = ["${aws_codepipeline.this.arn}"]
-  }
-}
-
-resource "aws_iam_policy" "eventbridge_start_pipeline" {
-  name        = "${local.project}-eventbridge-start-pipeline"
-  description = "EventBridge can start pipeline"
-  policy      = data.aws_iam_policy_document.eventbridge_start_pipeline.json
-}
-
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Attach policies to EventBridge role
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_iam_role_policy_attachment" "eventbridge_policy" {
-  for_each   = var.eventbridge_policy
-  role       = aws_iam_role.eventbridge_service_role.name
-  policy_arn = each.value
-}
-resource "aws_iam_role_policy_attachment" "eventbridge_start_pipeline" {
-  role       = aws_iam_role.eventbridge_service_role.name
-  policy_arn = aws_iam_policy.eventbridge_start_pipeline.arn
-}
