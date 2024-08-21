e40c10ea941dabb1de9c32facd288657e793910d
MagenX <magenx@users.noreply.github.com>
2024-08-21 19:43:19 +0200

    Update codecommit.tf

--- codecommit.tf
+++ codecommit.tf
@@ -7,7 +7,7 @@
 # Create CodeCommit repository for application code
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_codecommit_repository" "app" {
-  repository_name = var.app["domain"]
+  repository_name = var.magento["domain"]
   description     = "Magento 2.x code for ${local.project}"
   tags = {
     Name = "${local.project}"
@@ -17,8 +17,8 @@ resource "aws_codecommit_repository" "app" {
   command = <<EOF
           mkdir -p /tmp/magento && cd /tmp/magento
           git init
-          git config --global user.name "${var.app["admin_firstname"]}"
-          git config --global user.email "${var.app["admin_email"]}"
+          git config --global user.name "${var.magento["admin_firstname"]}"
+          git config --global user.email "${var.magento["admin_email"]}"
           git remote set-url origin codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.app.repository_name}
           git commit --allow-empty -m "init"
           git branch -m main
