07393eab471ea5f7c065c0473402c54e1061e5d7
MagenX <magenx@users.noreply.github.com>
2024-08-21 20:15:53 +0200

    Update user_data.tf

--- user_data.tf
+++ user_data.tf
@@ -7,7 +7,7 @@
 # Create SSM Document to configure EC2 instances in Auto Scaling Group
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_ssm_document" "user_data" {
-  for_each      = var.ec2
+  for_each      = toset(["frontend","admin"])
   name          = "BootstrappingEC2WithUserData${each.key}"
   document_format = "YAML"
   document_type = "Command"
@@ -18,15 +18,28 @@ mainSteps:
   - name: "BootstrappingEC2${each.key}"
     action: "aws:runShellScript"
     inputs:
-      runCommand: 
-${each.key == "varnish" ? local.varnish_user_data : local.frontend_user_data}
+      runCommand: <<EOF
+        - |-
+          AWSTOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
+          INSTANCE_LOCAL_IP=$(curl -s -H "X-aws-ec2-metadata-token: $${AWSTOKEN}" http://169.254.169.254/latest/meta-data/local-ipv4)
+        - |-
+          cd /home/${var.magento["brand"]}/public_html/
+          su ${var.magento["brand"]} -s /bin/bash -c "git init -b main"
+          su ${var.magento["brand"]} -s /bin/bash -c "git remote add origin codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.app.repository_name}"
+          su ${var.magento["brand"]} -s /bin/bash -c "git fetch origin main"
+          su ${var.magento["brand"]} -s /bin/bash -c "git reset origin/main --hard"
+        - |-
+          sed -i "s/listen 80;/listen $${INSTANCE_LOCAL_IP} 80;/" /etc/nginx/sites-available/${var.magento["domain"]}.conf
+          sed -i "s/localhost/$${INSTANCE_LOCAL_IP}/g" /etc/varnish/default.vcl
+        - |-
+          systemctl restart varnish nginx php${var.magento["php_version"]}-fpm
 EOF
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create SSM Document association with Auto Scaling Group
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_ssm_association" "user_data" {
-  for_each = var.ec2
+  for_each = toset(["frontend","admin"])
   name     = aws_ssm_document.user_data[each.key].name
   targets {
     key    = "tag:aws:autoscaling:groupName"
@@ -39,35 +52,4 @@ resource "aws_ssm_association" "user_data" {
   }
   association_name = "User-Data-for-EC2-instances-in-${aws_autoscaling_group.this[each.key].name}"
   document_version = "$LATEST"
-}
-
-locals {
-
-frontend_user_data = <<EOF
-        - |-
-          AWSTOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
-          INSTANCE_LOCAL_IP=$(curl -s -H "X-aws-ec2-metadata-token: $${AWSTOKEN}" http://169.254.169.254/latest/meta-data/local-ipv4)
-        - |-
-          cd /home/${var.app["brand"]}/public_html/
-          su ${var.app["brand"]} -s /bin/bash -c "git init -b main"
-          su ${var.app["brand"]} -s /bin/bash -c "git remote add origin codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.app.repository_name}"
-          su ${var.app["brand"]} -s /bin/bash -c "git fetch origin main"
-          su ${var.app["brand"]} -s /bin/bash -c "git reset origin/main --hard"
-        - |-
-          sed -i "s/INSTANCE_LOCAL_IP/$${INSTANCE_LOCAL_IP}/" /etc/nginx/sites-available/magento.conf
-          systemctl restart nginx php${var.app["php_version"]}-fpm
-EOF
-
-
-varnish_user_data = <<EOF
-        - |-
-          AWSTOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
-          INSTANCE_LOCAL_IP=$(curl -s -H "X-aws-ec2-metadata-token: $${AWSTOKEN}" http://169.254.169.254/latest/meta-data/local-ipv4)
-        - |-
-          sed -i "s/INSTANCE_LOCAL_IP/$${INSTANCE_LOCAL_IP}/" /etc/varnish/default.vcl
-          sed -i "s/INSTANCE_LOCAL_IP/$${INSTANCE_LOCAL_IP}/" /etc/varnish/varnish.params
-          sed -i "s/INSTANCE_LOCAL_IP/$${INSTANCE_LOCAL_IP}/" /etc/nginx/nginx.conf
-        - |-
-          systemctl restart nginx varnish
-EOF
 }
