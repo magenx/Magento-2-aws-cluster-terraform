457577a2fcbbd46e4462646ba80631b7fdfc7243
MagenX <magenx@users.noreply.github.com>
2024-08-21 19:37:29 +0200

    Create ebs_volume.tf

---
+++ ebs_volume.tf
@@ -0,0 +1,18 @@
+
+
+
+//////////////////////////////////////////////////////[ MARIADB DATA EBS VOLUME ]/////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create EBS Volume for MariaDB data storage
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_ebs_volume" "mariadb_data" {
+  availability_zone = aws_subnet.this.0.id
+  size              = 250
+  type              = "gp3"
+  final_snapshot    = true
+  
+  tags = {
+    Name = "mariadb.${var.magento["brand"]}.internal"
+  }
+}
