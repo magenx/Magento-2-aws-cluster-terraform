9d164659b26dca225225f489a336dba4eb8c83fc
MagenX <magenx@users.noreply.github.com>
2024-08-23 14:03:00 +0200

    Update codecommit.tf

--- codecommit.tf
+++ codecommit.tf
@@ -27,3 +27,29 @@ resource "aws_codecommit_repository" "magento" {
 EOF
   }
 }
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CodeCommit repository for services configuration
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_codecommit_repository" "services" {
+  repository_name = "${local.project}-services-config"
+  description     = "EC2 linux and services configurations"
+    tags = {
+    Name = "${local.project}-services-config"
+  }
+  provisioner "local-exec" {
+  interpreter = ["/bin/bash", "-c"]
+  command = <<EOF
+          mkdir -p /tmp/nginx && cd /tmp/nginx
+          git init
+          git commit --allow-empty -m "main branch"
+          git branch -m main
+          git push codecommit::${data.aws_region.current.name}://${local.project}-services-config main
+          git branch -m nginx_admin
+          git add .
+          git commit -m "nginx_ec2_config"
+          git push codecommit::${data.aws_region.current.name}://${local.project}-services-config nginx_admin
+          git branch -m nginx_frontend
+          git push codecommit::${data.aws_region.current.name}://${local.project}-services-config nginx_frontend
+EOF
+  }
+}
