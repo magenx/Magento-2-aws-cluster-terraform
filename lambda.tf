701367555fbd79f3b4b759d96b77e0e90018bc58
MagenX <magenx@users.noreply.github.com>
2024-08-23 15:14:14 +0200

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -7,7 +7,7 @@
 # Create Lambda IAM role and attach policy permissions
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_cloudwatch_log_group" "lambda" {
-  name              = "/lambda/${aws_lambda_function.image_optimization.last_modified}"
+  name              = "/lambda/${aws_lambda_function.image_optimization.version}"
   retention_in_days = 7
 }
 
