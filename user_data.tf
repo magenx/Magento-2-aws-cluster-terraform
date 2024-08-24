0fe5c41ff4d3cf986b2dd48501694908d42ff16d
MagenX <magenx@users.noreply.github.com>
2024-08-24 18:25:02 +0200

    Update user_data.tf

--- user_data.tf
+++ user_data.tf
@@ -17,7 +17,7 @@ mainSteps:
   - name: "BootstrappingEC2"
     action: "aws:runShellScript"
     inputs:
-      runCommand: <<EOF
+      runCommand:
         - |-
           AWSTOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
           INSTANCE_IP=$(curl -s -H "X-aws-ec2-metadata-token: $${AWSTOKEN}" http://169.254.169.254/latest/meta-data/local-ipv4)
