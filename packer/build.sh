f706b12b5225898aad4dfc5a47b37bb766a65df6
MagenX <magenx@users.noreply.github.com>
2024-08-30 18:47:55 +0200

    Update build.sh

--- packer/build.sh
+++ packer/build.sh
@@ -875,6 +875,7 @@ apt-get autoremove --purge -y
 
 echo "PS1='\[\e[37m\][\[\e[m\]\[\e[32m\]\u\[\e[m\]\[\e[37m\]@\[\e[m\]\[\e[35m\]\h\[\e[m\]\[\e[37m\]:\[\e[m\]\[\e[36m\]\W\[\e[m\]\[\e[37m\]]\[\e[m\]$ '" >> /etc/bashrc
 chmod +x /usr/local/bin/*
+chmod 700 /usr/bin/aws
 
 ## simple installation stats
 curl --silent -X POST https://www.magenx.com/ping_back_id_${INSTANCE_NAME}_domain_${parameter["DOMAIN"]}_geo_${parameter["TIMEZONE"]}_keep_30d >/dev/null 2>&1
