a53ef0f672b9604990034641ccb2e1550d2aafa7
MagenX <magenx@users.noreply.github.com>
2024-09-06 12:11:23 +0200

    Update packer.tf

--- packer.tf
+++ packer.tf
@@ -11,16 +11,17 @@ resource "null_resource" "packer" {
   for_each = var.ec2
   provisioner "local-exec" {
     working_dir = "${abspath(path.root)}/packer"
-    command = <<EOF
+    command = <<END
 /usr/bin/packer init -force ${abspath(path.root)}/packer/packer.pkr.hcl
-PACKER_LOG=1 PACKER_LOG_PATH="packerlog" /usr/bin/packer build \
+PACKER_LOG=1 PACKER_LOG_PATH="packer-${each.key}.log" /usr/bin/packer build \
 -var INSTANCE_NAME=${each.key} \
 -var SERVICE_ID=${aws_service_discovery_service.this[each.key].id} \
 -var VOLUME_SIZE=${each.value.volume_size} \
+-var MARIADB_DATA_VOLUME=${aws_ebs_volume.mariadb_data.id} \
 -var IAM_INSTANCE_PROFILE=${aws_iam_instance_profile.ec2[each.key].name} \
 -var PARAMETERSTORE_NAME=${aws_ssm_parameter.env.name} \
 packer.pkr.hcl
-EOF
+END
   }
  triggers = {
     ami_creation_date = data.aws_ami.distro.creation_date
