080e7258660179d32fe1b3f7127148d5b6edf0ed
MagenX <magenx@users.noreply.github.com>
2024-09-06 21:03:55 +0200

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -162,7 +162,7 @@ resource "aws_cloudfront_distribution" "this" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.default.arn
+    acm_certificate_arn = aws_acm_certificate.cloudfront.arn
     ssl_support_method  = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
