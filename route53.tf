29fa8d69b7ccbdc0b77e8d83c9ca5fba3a7ead11
MagenX <magenx@users.noreply.github.com>
2024-09-06 21:40:03 +0200

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -9,10 +9,15 @@
 resource "aws_route53_zone" "main" {
   name = var.magento["domain"]
 }
+
 resource "aws_route53_record" "cname" {
-  zone_id = aws_route53_zone.main.zone_id
+  zone_id = aws_route53_zone.main.id
   name    = var.magento["domain"]
-  type    = "CNAME"
-  ttl     = 300
-  records = [aws_cloudfront_distribution.this.domain_name]
+  type    = "A"
+  
+  alias {
+    name                   = aws_cloudfront_distribution.this.domain_name
+    zone_id                = aws_cloudfront_distribution.this.hosted_zone_id
+    evaluate_target_health = true
+  }
 }
