1e0a8957e067e16add8a684614aab452bcc4bf4a
MagenX <magenx@users.noreply.github.com>
2024-09-18 15:51:40 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,17 +12,12 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 3.0"
-    }
     external = {
       source  = "hashicorp/external"
       version = "~> 2.0"
     }
   }
 }
-
 
 provider "aws" {
   alias  = "useast1"
@@ -38,6 +33,4 @@ provider "aws" {
 }
 provider "null" {}
 provider "random" {}
-provider "template" {}
 provider "external" {}
-
