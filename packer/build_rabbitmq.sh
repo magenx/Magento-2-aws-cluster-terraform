94df79f64b23ced32120c03bcfedb3750acb7731
MagenX <magenx@users.noreply.github.com>
2024-09-19 12:22:21 +0200

    Create build_rabbitmq.sh

---
+++ packer/build_rabbitmq.sh
@@ -0,0 +1,84 @@
+#!bin/bash
+
+. /tmp/build_header.sh
+
+###################################################################################
+###                               RABBITMQ CONFIGURATION                        ###
+###################################################################################
+
+if [ "${INSTANCE_NAME}" == "rabbitmq" ]; then
+# RABBITMQ INSTALLATION
+curl -1sLf 'https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/setup.deb.sh' | bash
+curl -1sLf 'https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/setup.deb.sh' | bash
+apt -qq -y install rabbitmq-server=${RABBITMQ_VERSION}
+
+systemctl stop rabbitmq-server
+systemctl stop epmd*
+epmd -kill
+
+cat > /etc/rabbitmq/rabbitmq-env.conf <<END
+NODENAME=rabbit@localhost
+NODE_IP_ADDRESS=127.0.0.1
+ERL_EPMD_ADDRESS=127.0.0.1
+PID_FILE=/var/lib/rabbitmq/mnesia/rabbitmq_pid
+END
+
+echo '[{kernel, [{inet_dist_use_interface, {127,0,0,1}}]},{rabbit, [{tcp_listeners, [{"127.0.0.1", 5672}]}]}].' > /etc/rabbitmq/rabbitmq.config
+
+cat >> /etc/sysctl.conf <<END
+net.ipv6.conf.lo.disable_ipv6 = 0
+END
+
+sysctl -q -p
+
+cat > /etc/systemd/system/epmd.service <<END
+[Unit]
+Description=Erlang Port Mapper Daemon
+After=network.target
+Requires=epmd.socket
+
+[Service]
+ExecStart=/usr/bin/epmd -address 127.0.0.1 -daemon
+Type=simple
+StandardOutput=journal
+StandardError=journal
+User=epmd
+Group=epmd
+
+[Install]
+Also=epmd.socket
+WantedBy=multi-user.target
+END
+
+cat > /etc/systemd/system/epmd.socket <<END
+[Unit]
+Description=Erlang Port Mapper Daemon Activation Socket
+
+[Socket]
+ListenStream=4369
+BindIPv6Only=both
+Accept=no
+
+[Install]
+WantedBy=sockets.target
+END
+
+systemctl daemon-reload
+systemctl start rabbitmq-server
+rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbitmq_pid
+sleep 5
+
+# delete guest user
+rabbitmqctl delete_user guest
+
+# generate rabbitmq user permissions
+rabbitmqctl add_user ${parameter["BRAND"]} ${parameter["RABBITMQ_PASSWORD"]}
+rabbitmqctl set_permissions -p / ${parameter["BRAND"]} ".*" ".*" ".*"
+
+apt-mark hold erlang rabbitmq-server
+
+fi
+
+###################################################################################
+
+. /tmp/build_footer.sh
