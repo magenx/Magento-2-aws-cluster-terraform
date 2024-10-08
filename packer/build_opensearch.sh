bd4adb1f664e6b015ea126ee487ab20446782b90
MagenX <magenx@users.noreply.github.com>
2024-10-08 16:55:18 +0200

    Update build_opensearch.sh

--- packer/build_opensearch.sh
+++ packer/build_opensearch.sh
@@ -55,9 +55,14 @@ plugins.security.system_indices.indices: [".plugins-ml-config", ".plugins-ml-con
 
 END
 
-## OpenSearch settings
-sed -i "s/.*-Xms.*/-Xms512m/" /etc/opensearch/jvm.options
-sed -i "s/.*-Xmx.*/-Xmx1024m/" /etc/opensearch/jvm.options
+## OpenSearch jvm options
+HEAP_SIZE=$(echo "0.50*$(awk '/MemTotal/ { print $2 / (1024*1024)}' /proc/meminfo | cut -d'.' -f1)" | bc | xargs printf "%1.0f")
+if [ "${HEAP_SIZE}" == "0" ]; then HEAP_SIZE=1; fi
+
+cat <<END > /etc/opensearch/jvm.options.d/${parameter["BRAND"]}.options
+-Xms${HEAP_SIZE}g
+-Xmx${HEAP_SIZE}g
+END
 
 chown -R :opensearch /etc/opensearch/*
 systemctl daemon-reload
