338bbec2439642e12575e45514526c54863987a8
MagenX <magenx@users.noreply.github.com>
2024-10-20 17:55:08 +0200

    Update codepipeline.tf

--- codepipeline.tf
+++ codepipeline.tf
@@ -316,21 +316,6 @@ resource "aws_codebuild_project" "this" {
       type  = "PLAINTEXT"
   }
   environment_variable {
-      name  = "MAGENTO_ENV"
-      value = "${aws_ssm_parameter.magento_env.name}"
-      type  = "PARAMETER_STORE"
-  }
-  environment_variable {
-      name  = "COMPOSER_AUTH"
-      value = "${aws_ssm_parameter.composer_auth.name}"
-      type  = "PARAMETER_STORE"
-  }
-  environment_variable {
-      name  = "CODEDEPLOY_APPSPEC"
-      value = "${aws_ssm_parameter.codedeploy_appspec.name}"
-      type  = "PARAMETER_STORE"
-  }
-  environment_variable {
       name  = "PHP_VERSION"
       value = "${var.php_version}"
       type  = "PLAINTEXT"
