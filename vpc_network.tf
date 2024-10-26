570b4c6ea51d4ea96176ed40b1741014e4a2bcc0
MagenX <magenx@users.noreply.github.com>
2024-10-26 17:53:53 +0200

    Update vpc_network.tf

--- vpc_network.tf
+++ vpc_network.tf
@@ -7,10 +7,10 @@
 # Create our dedicated VPC
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_vpc" "this" {
-  cidr_block           = var.magento["cidr_block"]
-  instance_tenancy     = "default"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  cidr_block           = var.vpc["cidr_block"]
+  instance_tenancy     = var.vpc["instance_tenancy"]
+  enable_dns_support   = var.vpc["enable_dns_support"]
+  enable_dns_hostnames = var.vpc["enable_dns_hostnames"]
   tags = {
     Name = "${local.project}-vpc"
   }
