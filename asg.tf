7811b29e85eaea3f1ba0fe1756a3bafd8c45aada
MagenX <magenx@users.noreply.github.com>
2024-11-25 15:36:34 +0100

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -57,9 +57,41 @@ resource "aws_autoscaling_group" "this" {
   health_check_grace_period = var.asg["health_check_grace_period"]
   health_check_type         = var.asg["health_check_type"]
   target_group_arns  = [aws_lb_target_group.this[each.key].arn]
-  launch_template {
-    name    = aws_launch_template.this[each.key].name
-    version = "$Latest"
+  capacity_rebalance = each.value.max_size > 1 ? true : false
+  dynamic "launch_template" {
+    for_each = each.value.max_size > 1 ? [] : [1]
+    content {
+      id      = aws_launch_template.example[each.key].id
+      version = "$Latest"
+    }
+  }
+  dynamic "mixed_instances_policy" {
+    for_each = each.value.max_size > 1 ? [1] : []
+    instances_distribution {
+      on_demand_base_capacity                  = 1
+      on_demand_percentage_above_base_capacity = 0
+      spot_allocation_strategy                 = "capacity-optimized"
+    }
+    launch_template {
+      launch_template_specification {
+        launch_template_id = aws_launch_template.example.id
+        version            = "$Latest"
+      }
+    override {
+      instance_requirements {
+        vcpus {
+          min = 4
+          max = 8
+        }
+        memory_mib {
+          min = 8192
+          max = 16384
+        }
+        cpu_manufacturers      = ["amazon-web-services"] # Graviton
+        allowed_instance_types = ["*g.*"] # Exclude t-series if needed
+      }
+     }
+   }
   }
   instance_refresh {
     strategy = "Rolling"
