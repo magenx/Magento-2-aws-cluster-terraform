076b98e07f51d7ed622f81508edde965711ebf01
MagenX <magenx@users.noreply.github.com>
2024-12-23 18:31:07 +0100

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -20,16 +20,6 @@ resource "aws_cloudfront_origin_access_control" "this" {
     origin_access_control_origin_type = "lambda"
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
-# Create CloudFront function
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_cloudfront_function" "this" {
-  publish = true
-  name    = "${local.project}-urlrewrite"
-  comment = "UrlRewrite function for ${local.project} images optimization"
-  runtime = "cloudfront-js-2.0"
-  code    = file("${abspath(path.root)}/cloudfront/index.js")
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
 # Create a custom CloudFront Response Headers Policy
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_cloudfront_response_headers_policy" "media" {
