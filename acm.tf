695e8b5ac47cd64f8201e63efc6ea01cd6f6f2df
MagenX <magenx@users.noreply.github.com>
2024-12-23 18:37:18 +0100

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -10,6 +10,20 @@ resource "aws_acm_certificate" "default" {
   domain_name               = "${var.domain}"
   subject_alternative_names = ["*.${var.domain}"]
   validation_method         = "EMAIL"
+  lifecycle {
+    create_before_destroy   = true
+  }
+  tags = {
+    Name = "${local.project}-${var.domain}-cert"
+  }
+}
+
+resource "aws_acm_certificate" "cloudfront" {
+  count                     = data.aws_region.current.name == "us-east-1" ? 0 : 1
+  provider                  = aws.useast1
+  domain_name               = "${var.domain}"
+  subject_alternative_names = ["*.${var.domain}"]
+  validation_method         = "EMAIL"
 
   lifecycle {
     create_before_destroy   = true
@@ -23,4 +37,10 @@ resource "aws_acm_certificate" "default" {
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_acm_certificate_validation" "default" {
   certificate_arn = aws_acm_certificate.default.arn
+}
+
+resource "aws_acm_certificate_validation" "cloudfront" {
+  count           = data.aws_region.current.name == "us-east-1" ? 0 : 1
+  provider        = aws.useast1
+  certificate_arn = aws_acm_certificate.cloudfront[0].arn
 }
