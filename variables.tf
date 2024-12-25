12912de307635e7fce15bca4c0ba5f3a122f6db1
MagenX <magenx@users.noreply.github.com>
2024-12-25 17:13:59 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -63,7 +63,7 @@ variable "vpc" {
 variable "ec2" {
   default = {
     frontend = {
-      instance_type    = "c8g.xlarge"
+      instance_type    = "m8g.large"
       service          = null
       volume_size      = "25"
       desired_capacity = "1"
