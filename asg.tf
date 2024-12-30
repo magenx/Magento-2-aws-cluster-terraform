d32d9f0f9bbba9b73cd3fff340b7cd163b89999c
MagenX <magenx@users.noreply.github.com>
2024-12-30 12:39:46 +0100

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -52,6 +52,16 @@ resource "aws_launch_template" "this" {
       )
     }
   }
+  user_data = base64encode(<<EOF
+#!/bin/bash
+mkdir /tmp/ssm
+cd /tmp/ssm
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_arm64/amazon-ssm-agent.deb
+dpkg -i amazon-ssm-agent.deb
+systemctl enable amazon-ssm-agent
+systemctl start amazon-ssm-agent
+EOF
+  )
   metadata_options {
     http_endpoint               = "enabled"
     http_tokens                 = "required"
