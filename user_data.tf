e2b07935ea124571f14d6b751e33be1c27ab49b1
MagenX <magenx@users.noreply.github.com>
2024-12-30 12:22:17 +0100

    Update user_data.tf

--- user_data.tf
+++ user_data.tf
@@ -153,7 +153,7 @@ mainSteps:
           wget https://amazoncloudwatch-agent.s3.amazonaws.com/debian/arm64/latest/amazon-cloudwatch-agent.deb
           dpkg -i amazon-cloudwatch-agent.deb
           /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:amazon-cloudwatch-agent-$${INSTANCE_NAME}.json
-  - name: "InstallCloudWatchAgent"
+  - name: "CloudMapInstanceRegistration"
     action: "aws:runShellScript"
     inputs:
       runCommand:
