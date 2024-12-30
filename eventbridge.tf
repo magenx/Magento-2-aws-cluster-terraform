ac5149e257e5c45da9c53003d0cbe63ca266fb10
MagenX <magenx@users.noreply.github.com>
2024-12-30 11:24:20 +0100

    Update eventbridge.tf

--- eventbridge.tf
+++ eventbridge.tf
@@ -16,9 +16,52 @@ data "aws_iam_policy_document" "eventbridge_assume_role" {
     }
   }
 }
-
 resource "aws_iam_role" "eventbridge_service_role" {
   name = "${local.project}-EventBridgeServiceRole"
   description = "Provides EventBridge manage events on your behalf."
   assume_role_policy = data.aws_iam_policy_document.eventbridge_assume_role.json
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create SSM Document association with Auto Scaling Group
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_ssm_association" "user_data" {
+  for_each = var.ec2
+  name     = aws_ssm_document.user_data.name
+  targets {
+    key    = "tag:aws:autoscaling:groupName"
+    values = [aws_autoscaling_group.this[each.key].name]
+  }
+  association_name = "Configuration-for-EC2-instances-in-${aws_autoscaling_group.this[each.key].name}"
+  document_version = "$LATEST"
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# EventBridge Rule for S3 bucket object event
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_event_rule" "s3_update" {
+  for_each    = var.ec2
+  name        = "${local.project}-${each.key}-s3-update-setup"
+  description = "Trigger SSM document when s3 system bucket updated for ${each.key}"
+  event_pattern = jsonencode({
+    "source"       : ["aws.s3"],
+    "detail-type"  : ["Object Created"],
+    "detail"       : {
+      "bucket"     : { "name" : [aws_s3_bucket.this["system"].bucket] },
+      "object"     : { "key" : [{ "prefix" : "setup/" }] }
+    }
+  })
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# EventBridge Rule Target for SSM Document
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_event_target" "instance_setup" {
+  depends_on = [aws_autoscaling_group.this]
+  for_each  = var.ec2
+  rule      = aws_cloudwatch_event_rule.s3_update[each.key].name
+  target_id = "${local.project}-${each.key}-instance-setup"
+  arn       =  aws_ssm_document.user_data.arn
+  role_arn  =  aws_iam_role.ec2[each.key].arn
+  run_command_targets {
+    key    = "tag:Name"
+    values = ["${local.project}-${each.key}-ec2"]
+  }
 }
