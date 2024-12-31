23144df7bda42c46a7dd1ae7306b0894006dbf2a
MagenX <magenx@users.noreply.github.com>
2024-12-31 12:22:36 +0100

    Update aws_config.tf

--- aws_config.tf
+++ aws_config.tf
@@ -29,8 +29,7 @@ resource "aws_config_config_rule" "this" {
 # Create AWS Config recorder
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_config_configuration_recorder" "this" {
-  for_each = data.aws_config_configuration_recorder_status.existing.recorders == [] ? 
-              { "default" = "recorder" } : {}
+  count    = data.aws_config_configuration_recorder_status.existing.recorders == [] ? 1 : 0
   name     = each.key
   role_arn = aws_iam_role.config.arn
   recording_group {
