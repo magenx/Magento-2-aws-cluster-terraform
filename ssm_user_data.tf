90960b2e1eedc03948d169eac8df5893304bd135
MagenX <magenx@users.noreply.github.com>
2025-01-05 16:23:14 +0100

    Update ssm_user_data.tf

--- ssm_user_data.tf
+++ ssm_user_data.tf
@@ -30,6 +30,13 @@ parameters:
   instanceId:
     type: String
 mainSteps:
+  - name: "SendNotification"
+    action: "aws:executeAwsApi"
+    inputs:
+      Service: "sns"
+      Api: "Publish"
+      TopicArn: ${aws_sns_topic.default.arn}
+      Message: "Init EC2 with user_data @ {{ instanceId }}"
   - name: ExecuteInitEC2WithUserData
     action: aws:runCommand
     inputs:
@@ -49,17 +56,7 @@ resource "aws_ssm_document" "user_data" {
   content = <<EOF
 schemaVersion: "2.2"
 description: "Init EC2 instance with UserData"
-parameters:
-  instanceId:
-    type: String
 mainSteps:
-  - name: "SendNotification"
-    action: "aws:executeAwsApi"
-    inputs:
-      Service: "sns"
-      Api: "Publish"
-      TopicArn: ${aws_sns_topic.default.arn}
-      Message: "Init EC2 with user_data @ {{ instanceId }}"
   - name: "WebStackCleanup"
     action: "aws:runShellScript"
     inputs:
