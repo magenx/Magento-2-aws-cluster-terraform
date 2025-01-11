d090b819e698547f1ecd8cbc28e1e1d338226180
MagenX <magenx@users.noreply.github.com>
2025-01-11 20:25:49 +0100

    Update eventbridge.tf

--- eventbridge.tf
+++ eventbridge.tf
@@ -79,8 +79,8 @@ resource "aws_cloudwatch_event_target" "s3_update" {
     arn = aws_sqs_queue.dead_letter_queue.arn
   }
   run_command_targets {
-    key    = "tag:Brand"
-    values = [var.brand]
+    key    = "tag:Config"
+    values = ["s3_system_setup"]
   }
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
