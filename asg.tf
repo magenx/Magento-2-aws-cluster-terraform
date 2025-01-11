816ebfc23e4ba210bca595991e19a1004648f04c
MagenX <magenx@users.noreply.github.com>
2025-01-11 19:59:02 +0100

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -100,7 +100,7 @@ resource "aws_autoscaling_group" "this" {
   max_size            = each.value.max_size
   health_check_grace_period = var.asg["health_check_grace_period"]
   health_check_type         = var.asg["health_check_type"]
-  target_group_arns  = [aws_lb_target_group.this.arn]
+  target_group_arns  = each.key == "varnish" ? [aws_lb_target_group.this.arn] : []
   launch_template {
       id      = aws_launch_template.this[each.key].id
       version = "$Latest"
