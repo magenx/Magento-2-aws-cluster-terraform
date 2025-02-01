7e91fccae7116de90106d5034ffe4e7be9b6395f
MagenX <magenx@users.noreply.github.com>
2025-02-01 13:21:11 +0100

    Update aws_budget.tf

--- aws_budget.tf
+++ aws_budget.tf
@@ -29,3 +29,33 @@ resource "aws_budgets_budget" "all" {
     subscriber_sns_topic_arns  = [aws_sns_topic.default.arn]
   }
 }
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create alert when your Cost Anomaly Detection trigger changes
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_ce_anomaly_monitor" "cost" {
+  name              = "${local.project}-cost-anomaly-detection"
+  monitor_type      = "DIMENSIONAL"
+  monitor_dimension = "SERVICE"
+  tags = {
+    Name = "${local.project}-cost-anomaly-detection"
+    }
+}
+
+resource "aws_ce_anomaly_subscription" "cost_alert" {
+  name      = "${local.project}-cost-anomaly-alert"
+  frequency = "IMMEDIATE"
+  threshold_expression {
+      dimension {
+        key           = "ANOMALY_TOTAL_IMPACT_PERCENTAGE"
+        match_options = ["GREATER_THAN_OR_EQUAL"]
+        values        = ["15"]
+      }
+    }
+  monitor_arn_list = [
+    aws_ce_anomaly_monitor.cost.arn
+  ]
+  subscriber {
+    type    = "SNS"
+    address = aws_sns_topic.default.arn
+  }
+}
