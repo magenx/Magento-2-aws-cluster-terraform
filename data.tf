fab28efb75ddacc171104465eac0bc73903a2fd3
MagenX <magenx@users.noreply.github.com>
2025-02-02 23:45:32 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -30,8 +30,8 @@ data "aws_availability_zones" "available" {
   state = "available"
   exclude_zone_ids = ["use1-az3"]
 }
-data "aws_availability_zone" "one" {
-  for_each = toset(slice(tolist(data.aws_availability_zones.available.names), 0, var.vpc["availability_zones_qty"]))
+data "aws_availability_zone" "available" {
+  for_each = toset(data.aws_availability_zones.available.names)
   name = each.key
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
