bba5f4c5bbda84ff1ca31331773800661cc3ece1
MagenX <magenx@users.noreply.github.com>
2025-02-05 23:21:52 +0100

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -19,7 +19,7 @@ resource "aws_acm_certificate" "default" {
 }
 
 resource "aws_acm_certificate" "cloudfront" {
-  count                     = data.aws_region.current.name == "us-east-1" ? 0 : 1
+  for_each = data.aws_region.current.name == "us-east-1" ? {} : { "this" = var.domain }
   provider                  = aws.useast1
   domain_name               = "${var.domain}"
   subject_alternative_names = ["*.${var.domain}"]
@@ -40,7 +40,7 @@ resource "aws_acm_certificate_validation" "default" {
 }
 
 resource "aws_acm_certificate_validation" "cloudfront" {
-  count           = data.aws_region.current.name == "us-east-1" ? 0 : 1
+  for_each = data.aws_region.current.name == "us-east-1" ? {} : { "this" = aws_acm_certificate.cloudfront["this"] }
   provider        = aws.useast1
-  certificate_arn = aws_acm_certificate.cloudfront[0].arn
+  certificate_arn = each.value.arn
 }
