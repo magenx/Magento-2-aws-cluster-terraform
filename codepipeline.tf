b9a46aacf09b8eb2cf2676dce85a2400ce801c85
MagenX <magenx@users.noreply.github.com>
2025-02-15 18:50:43 +0100

    Create codepipeline.tf

---
+++ codepipeline.tf
@@ -0,0 +1,341 @@
+
+
+
+////////////////////////////////////////////////////////[ CODEPIPELINE ]//////////////////////////////////////////////////
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CodeDeploy role
+# # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_iam_policy_document" "codedeploy_assume_role" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codedeploy.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "codedeploy" {
+  name        = "${local.project}-codedeploy-role"
+  description = "Allows CodeDeploy to call AWS services on your behalf."
+  assume_role_policy = data.aws_iam_policy_document.codedeploy_assume_role.json
+  tags = {
+    Name = "${local.project}-codedeploy-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "AWSCodeDeployRole" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+  role       = aws_iam_role.codedeploy.name
+}
+
+data "aws_iam_policy_document" "codedeploy" {
+  statement {
+    sid    = "AllowCodeDeployToASG"
+    effect = "Allow"
+    actions = [
+      "autoscaling:CompleteLifecycleAction",
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:PutLifecycleHook",
+      "autoscaling:DeleteLifecycleHook",
+      "autoscaling:RecordLifecycleActionHeartbeat"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "codedeploy" {
+  role   = aws_iam_role.codedeploy.name
+  policy = data.aws_iam_policy_document.codedeploy.json
+}
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CodeBuild role
+# # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_iam_policy_document" "codebuild_assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "codebuild" {
+  name               = "${local.project}-codebuild-role"
+  assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role.json
+  tags = {
+    Name = "${local.project}-codebuild-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild" {
+  role       = aws_iam_role.codebuild.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
+}
+
+data "aws_iam_policy_document" "codebuild" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "codepipeline:PollForJobs",
+      "codepipeline:GetPipelineExecution",
+      "codepipeline:GetPipeline",
+      "codepipeline:ListPipelineExecutions",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeVpcs",
+      "ec2:DeleteNetworkInterface",
+      "ec2:CreateNetworkInterface",
+      "ec2:CreateNetworkInterfacePermission"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "codebuild" {
+  name   = "CodeBuildCustomPolicy"
+  role   = aws_iam_role.codebuild.id
+  policy = data.aws_iam_policy_document.codebuild.json
+}
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CodePipeline role
+# # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_iam_policy_document" "codepipeline_assume_role" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "codepipeline" {
+  name        = "${local.project}-codepipeline-role"
+  description = "Allows CodePipeline to call AWS services on your behalf."
+  assume_role_policy = data.aws_iam_policy_document.codepipeline_assume_role.json
+  tags = {
+    Name = "${local.project}-codepipeline-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline" {
+  role       = aws_iam_role.codepipeline.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployFullAccess"
+}
+
+data "aws_iam_policy_document" "codepipeline" {
+  statement {
+    sid       = "AllowCodeDeploySNSAlertTrigger"
+    effect    = "Allow"
+    actions   = ["sns:Publish"]
+    resources = [aws_sns_topic.default.arn]
+  }
+  statement {
+    sid       = "AllowCodeBuildActions"
+    effect    = "Allow"
+    actions   = [
+       "codebuild:StartBuild",
+       "codebuild:StartBuildBatch",
+       "codebuild:BatchGetBuilds",
+       "codebuild:BatchGetBuildBatches"
+       ]
+    resources = ["${aws_codebuild_project.this.arn}"]
+ }
+}
+
+resource "aws_iam_role_policy" "codepipeline" {
+  name   = "${local.project}-codepipeline-policy"
+  role   = aws_iam_role.codepipeline.id
+  policy = data.aws_iam_policy_document.codepipeline.json
+}
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# CodePipeline to pull new release from GitHub and deploy to ASG instances
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_codepipeline" "this" {
+  name          = "${local.project}-pipeline"
+  role_arn      = aws_iam_role.codepipeline.arn
+  pipeline_type = "V2"
+  artifact_store {
+    location    = aws_s3_bucket.this["system"].bucket
+    type        = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "S3_Source"
+      namespace        = "SourceVariables"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "S3"
+      version          = "1"
+      output_artifacts = ["source_output"]
+      configuration = {
+        S3Bucket             = aws_s3_bucket.this["system"].bucket
+        S3ObjectKey          = "releases/placeholder.zip"
+        AllowOverrideForS3ObjectKey = "true"
+        PollForSourceChanges = "false" 
+      }
+    }
+  }
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["complete_build_output"]
+      configuration = {
+        ProjectName = aws_codebuild_project.this.name
+        PrimarySource = "build_source_output"
+      }
+    }
+  }
+  stage {
+    name = "Deploy"
+  
+    action {
+      name     = "Deploy_Approval"
+      category = "Approval"
+      owner    = "AWS"
+      provider = "Manual"
+      version  = "1"
+      run_order = 1
+      configuration = {
+        NotificationArn = aws_sns_topic.default.arn
+        CustomData      = "Approve codepipeline [#{codepipeline.PipelineExecutionId}] Deploy action for ${local.project} [#{SourceVariables.VersionId}]"
+      }
+    }
+    dynamic "action" {
+      for_each = { for instance, value in var.ec2 : instance => value if value.service == null }
+      content {
+        name            = "Deploy_to_${action.key}_ASG"
+        category        = "Deploy"
+        owner           = "AWS"
+        version         = "1"
+        run_order       = 2
+        provider        = "CodeDeploy"
+        input_artifacts = ["complete_build_output"]
+        configuration = {
+          ApplicationName     = aws_codedeploy_app.this[action.key].name
+          DeploymentGroupName = aws_codedeploy_deployment_group.this[action.key].deployment_group_name
+        }
+      }
+    }
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CodeBuild project
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_codebuild_project" "this" {
+  badge_enabled          = false
+  build_timeout          = 60
+  description            = "${local.project}-codebuild-project"
+  name                   = "${local.project}-codebuild-project"
+  queued_timeout         = 300
+  depends_on             = [aws_iam_role.codebuild]
+  service_role           = aws_iam_role.codebuild.arn
+	
+  tags = {
+    Name = "${local.project}-codebuild-project"
+  }
+
+  artifacts {
+    encryption_disabled    = false
+    override_artifact_name = true
+    packaging              = "NONE"
+    type                   = "CODEPIPELINE"
+  }
+
+  cache {
+    modes = []
+    type  = "NO_CACHE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_LARGE"
+    image                       = "aws/codebuild/standard:7.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = false
+    type                        = "LINUX_CONTAINER"
+	  
+  environment_variable {
+      name  = "BRAND"
+      value = "${var.brand}"
+      type  = "PLAINTEXT"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = aws_cloudwatch_log_group.codebuild.name
+      stream_name = aws_cloudwatch_log_stream.codebuild.name
+      status      = "ENABLED"
+  }
+
+  s3_logs {
+      status = "DISABLED"
+    }
+  }
+
+  source {
+    type = "CODEPIPELINE"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# CodeDeploy Applications for frontend ASG
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_codedeploy_app" "this" {
+  for_each = { for instance, value in var.ec2 : instance => value if value.service == null }
+  name = "${local.project}-${each.key}-codedeploy-app"
+  compute_platform = "Server"
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# CodeDeploy Deployment Groups for ASGs
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_codedeploy_deployment_group" "this" {
+  for_each = { for instance, value in var.ec2 : instance => value if value.service == null }
+  deployment_group_name  = "${local.project}-${each.key}-deployment-group"
+  deployment_config_name = "CodeDeployDefault.AllAtOnce"
+  app_name               = aws_codedeploy_app.this[each.key].name
+  service_role_arn       = aws_iam_role.codedeploy.arn
+  autoscaling_groups    = [aws_autoscaling_group.this[each.key].name]
+  trigger_configuration {
+    trigger_events     = ["DeploymentStart","DeploymentSuccess","DeploymentFailure"]
+    trigger_name       = "${local.project}-${each.key}-deployment-notification"
+    trigger_target_arn = aws_sns_topic.default.arn
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create CloudWatch log group and log stream for CodeBuild logs
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_cloudwatch_log_group" "codebuild" {
+  name = "${local.project}-codebuild-project"
+  tags = {
+    Name = "${local.project}-codebuild-project"
+  }
+}
+resource "aws_cloudwatch_log_stream" "codebuild" {
+  name = "${local.project}-codebuild-project"
+  log_group_name = aws_cloudwatch_log_group.codebuild.name
+}
