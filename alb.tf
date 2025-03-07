1831bcc09da0912c3853a84f6bcec29b04c80d29
MagenX <magenx@users.noreply.github.com>
2025-03-07 20:07:26 +0100

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -47,7 +47,7 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.this.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-FS-1-2-Res-2020-10"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
   certificate_arn   = aws_acm_certificate.default.arn
   default_action {
     type             = "fixed-response"
