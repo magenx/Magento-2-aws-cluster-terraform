e450d36d3be1817613bf0b36fc136a0b75d96fa5
MagenX <magenx@users.noreply.github.com>
2025-03-08 22:17:26 +0100

    Update ssm_configuration.tf

--- ssm_configuration.tf
+++ ssm_configuration.tf
@@ -60,8 +60,8 @@ mainSteps:
         commands:
           - |-
             #!/bin/bash
-            pip3 install ansible-core --upgrade
-            ansible localhost -m ping > /dev/null 2>&1 && echo "SUCCESS: Ansible ping worked!" || { echo "ERROR: Ansible ping failed!"; exit 1; }
+            sudo pipx install ansible-core
+            sudo /root/.local/bin/ansible localhost -m ping > /dev/null 2>&1 && echo "SUCCESS: Ansible ping worked!" || { echo "ERROR: Ansible ping failed!"; exit 1; }
             INSTANCE_NAME=$(metadata tags/instance/InstanceName)
             INSTANCE_IP=$(metadata local-ipv4)
             SETUP_DIRECTORY="/opt/${var.brand}/setup"
@@ -69,8 +69,8 @@ mainSteps:
             mkdir -p "$${INSTANCE_DIRECTORY}"
             touch $${SETUP_DIRECTORY}/init
             S3_OPTIONS="--quiet --exact-timestamps --delete"
-            aws s3 sync "s3://${aws_s3_bucket.this["system"].bucket}/setup/$${INSTANCE_NAME}" "$${INSTANCE_DIRECTORY}" $${S3_OPTIONS}
-            ansible-playbook -i localhost -c local -e "SSM=True instance_name=$${INSTANCE_NAME} brand=${var.brand} instance_ip=$${INSTANCE_IP}" -v  $${INSTANCE_DIRECTORY}/$${INSTANCE_NAME}.yml
+            sudo /root/awscli/bin/aws s3 sync "s3://${aws_s3_bucket.this["system"].bucket}/setup/$${INSTANCE_NAME}" "$${INSTANCE_DIRECTORY}" $${S3_OPTIONS}
+            sudo /root/.local/bin/ansible-playbook -i localhost -c local -e "SSM=True instance_name=$${INSTANCE_NAME} brand=${var.brand} instance_ip=$${INSTANCE_IP}" -v  $${INSTANCE_DIRECTORY}/$${INSTANCE_NAME}.yml
       CloudWatchOutputConfig:
         CloudWatchLogGroupName: "${local.project}-InstanceConfiguration"
         CloudWatchOutputEnabled: true
