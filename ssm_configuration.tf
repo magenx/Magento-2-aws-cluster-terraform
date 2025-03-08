69fd462055ade7d5b16ebc2cb36bb51dbb520c45
MagenX <magenx@users.noreply.github.com>
2025-03-08 12:49:01 +0100

    Update ssm_configuration.tf

--- ssm_configuration.tf
+++ ssm_configuration.tf
@@ -14,30 +14,66 @@ resource "aws_ssm_document" "configuration" {
 schemaVersion: "0.3"
 description: "Runs Ansible Playbook to configure EC2 instance"
 parameters:
-  InstanceIds:
+  S3ObjectKey:
     type: String
-    description: The target instance ids
-    default: ""
-  EventSource:
+    description: "The S3 object key path"
+  AutomationAssumeRole:
     type: String
-    description: "Event source"
-    default: "aws.autoscaling"
+    description: "The ARN of the role that allows Automation to perform the actions"
 mainSteps:
-  - name: "RunAnsiblePlaybook"
+  - name: "ExtractInstanceName"
+    action: aws:executeScript
+    inputs:
+      Runtime: python3.11
+      Handler: extract_instance_name
+      Script: |-
+        import re
+        def extract_instance_name(event, context):
+            s3_object_key = event['S3ObjectKey']
+            instance_name = s3_object_key.split('/')[1]
+            return { "InstanceName": instance_name }
+      InputPayload:
+        S3ObjectKey: "{{ S3ObjectKey }}"
+    outputs:
+      - Name: InstanceName
+        Selector: "$.Payload.InstanceName"
+        Type: String
+  - name: "FilterInstancesByNameTag"
+    action: aws:executeAwsApi
+    inputs:
+      Service: ec2
+      Api: DescribeInstances
+      Filters:
+        - Name: tag:Name
+          Values:
+            - "{{ ExtractInstanceName.InstanceName }}"
+    outputs:
+      - Name: InstanceIds
+        Selector: "$.Reservations[].Instances[].InstanceId"
+        Type: StringList
+  - name: "RunCommandOnInstances"
     action: aws:runCommand
     inputs:
-      InstanceIds:
-        - "{{ InstanceIds }}"
-      DocumentName: AWS-ApplyAnsiblePlaybooks
+      DocumentName: "AWS-RunShellScript"
+      InstanceIds: "{{ FilterInstancesByNameTag.InstanceIds }}"
       Parameters:
-        SourceType: "S3"
-        SourceInfo:
-          path: "https://${aws_s3_bucket.this["system"].bucket_domain_name}/setup/"
-        InstallDependencies: "False"
-        ExtraVariables: "SSM=True"
-        Check: "False"
-        Verbose: "-v"
-        TimeoutSeconds: "120"
+        commands:
+          - |-
+            #!/bin/bash
+            pip3 install ansible-core --upgrade
+            ansible localhost -m ping > /dev/null 2>&1 && echo "SUCCESS: Ansible ping worked!" || { echo "ERROR: Ansible ping failed!"; exit 1; }
+            INSTANCE_NAME=$(metadata tags/instance/InstanceName)
+            INSTANCE_IP=$(metadata local-ipv4)
+            SETUP_DIRECTORY="/opt/${var.brand}/setup"
+            INSTANCE_DIRECTORY="$${SETUP_DIRECTORY}/$${INSTANCE_NAME}"
+            mkdir -p "$${INSTANCE_DIRECTORY}"
+            touch $${SETUP_DIRECTORY}/init
+            S3_OPTIONS="--quiet --exact-timestamps --delete"
+            aws s3 sync "s3://${aws_s3_bucket.this["system"].bucket}/setup/$${INSTANCE_NAME}" "$${INSTANCE_DIRECTORY}" $${S3_OPTIONS}
+            ansible-playbook -i localhost -c local -e "SSM=True instance_name=$${INSTANCE_NAME} brand=${var.brand} instance_ip=$${INSTANCE_IP}" -v  $${INSTANCE_DIRECTORY}/$${INSTANCE_NAME}.yml
+      CloudWatchOutputConfig:
+        CloudWatchLogGroupName: "${local.project}-InstanceConfiguration"
+        CloudWatchOutputEnabled: true
   - name: "SendExecutionLog"
     action: "aws:executeAwsApi"
     isEnd: true
