5a25e46a6f576e38d394bf4dccd8b05eb98a1a18
MagenX <magenx@users.noreply.github.com>
2025-03-09 14:55:28 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -180,11 +180,25 @@ data "aws_iam_policy_document" "system" {
     sid    = "AllowEC2S3Access"
     effect = "Allow"
     actions = [
-      "s3:ListBucket",
+      "s3:ListBucket"
+    ]
+    resources = [
+      "${aws_s3_bucket.this["system"].arn}","${aws_s3_bucket.this["system"].arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers =  values(aws_iam_role.ec2)[*].arn
+    }
+  }
+
+ statement {
+    sid    = "AllowEC2S3GetObject"
+    effect = "Allow"
+    actions = [
       "s3:GetObject"
     ]
     resources = [
-      "${aws_s3_bucket.this["system"].arn}/setup","${aws_s3_bucket.this["system"].arn}/setup/*"
+      "${aws_s3_bucket.this["system"].arn}/setup/*"
     ]
     principals {
       type        = "AWS"
