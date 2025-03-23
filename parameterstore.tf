f5a4b3c4cc86995ac9f52237c3219d8be3a93288
MagenX <magenx@users.noreply.github.com>
2025-03-23 17:14:01 +0100

    Update parameterstore.tf

--- parameterstore.tf
+++ parameterstore.tf
@@ -1,92 +1,71 @@
 
 
 
-/////////////////////////////////////////////////////////[ SYSTEMS MANAGER ]//////////////////////////////////////////////
+///////////////////////////////////////////////[ SYSTEMS MANAGER - PARAMETERSTORE ]///////////////////////////////////////
 
 # # ---------------------------------------------------------------------------------------------------------------------#
-# Create SSM Parameter store for aws params
+# Create SSM Parameterstore for aws env
 # # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_ssm_parameter" "env" {
-  name        = "${local.project}-${local.environment}-env"
-  description = "Environment variables for ${local.project} in ${data.aws_region.current.name}"
+resource "aws_ssm_parameter" "aws_env" {
+  for_each    = local.parameters
+  name        = "/${local.project}/${local.environment}/${each.key}"
+  description = "Environment variable value for ${each.key}"
   type        = "String"
-  value       = <<EOF
-{
-"PROJECT" : "${local.project}",
-"AWS_DEFAULT_REGION" : "${data.aws_region.current.name}",
-"VPC_ID" : "${aws_vpc.this.id}",
-"CIDR" : "${aws_vpc.this.cidr_block}",
-"SUBNET_ID" : "${values(aws_subnet.this).0.id}",
-"SECURITY_GROUP" : "${aws_security_group.ec2.id}",
-"SOURCE_AMI" : "${data.aws_ami.distro.id}",
-"VOLUME_SIZE" : "${var.asg["volume_size"]}",
-"ALB_DNS_NAME" : "${aws_lb.this.dns_name}",
-"EFS_SYSTEM_ID" : "${aws_efs_file_system.this.id}",
-"EFS_ACCESS_POINT_VAR" : "${aws_efs_access_point.this["var"].id}",
-"EFS_ACCESS_POINT_MEDIA" : "${aws_efs_access_point.this["media"].id}",
-"SNS_TOPIC_ARN" : "${aws_sns_topic.default.arn}",
-"CODECOMMIT_MAGENTO_REPO" : "codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.magento.repository_name}",
-"CODECOMMIT_SERVICES_REPO" : "codecommit::${data.aws_region.current.name}://${aws_codecommit_repository.services.repository_name}",
-"RABBITMQ_ENDPOINT" : "${regex("amqps://(.*):5671",aws_mq_broker.this.instances.0.endpoints.0)[0]}",
-"RABBITMQ_USER" : "${var.brand}",
-"RABBITMQ_PASSWORD" : "${random_password.this["rabbitmq"].result}",
-"OPENSEARCH_ENDPOINT" : "https://${aws_opensearch_domain.this.endpoint}:443",
-"OPENSEARCH_ADMIN" : "${random_string.this["opensearch"].result}",
-"OPENSEARCH_PASSWORD" : "${random_password.this["opensearch"].result}",
-"REDIS_CACHE_BACKEND" : "${aws_elasticache_replication_group.this["cache"].primary_endpoint_address}",
-"REDIS_SESSION_BACKEND" : "${aws_elasticache_replication_group.this["session"].primary_endpoint_address}",
-"REDIS_CACHE_BACKEND_RO" : "${aws_elasticache_replication_group.this["cache"].reader_endpoint_address}",
-"REDIS_SESSION_BACKEND_RO" : "${aws_elasticache_replication_group.this["session"].reader_endpoint_address}",
-"REDIS_PASSWORD" : "${random_password.this["redis"].result}",
-"S3_MEDIA_BUCKET" : "${aws_s3_bucket.this["media"].bucket}",
-"S3_MEDIA_BUCKET_URL" : "${aws_s3_bucket.this["media"].bucket_regional_domain_name}",
-"S3_MEDIA_BUCKET_KEY" : "${aws_iam_access_key.s3.id}",
-"S3_MEDIA_BUCKET_SECRET" : "${aws_iam_access_key.s3.secret}",
-"SES_KEY" : "${aws_iam_access_key.ses_smtp_user_access_key.id}",
-"SES_SECRET" : "${aws_iam_access_key.ses_smtp_user_access_key.secret}",
-"SES_PASSWORD" : "${aws_iam_access_key.ses_smtp_user_access_key.ses_smtp_password_v4}",
-"SES_ENDPOINT" : "email-smtp.${data.aws_region.current.name}.amazonaws.com",
-"DATABASE_ENDPOINT" : "${aws_db_instance.this.endpoint}",
-"DATABASE_NAME" : "${aws_db_instance.this.identifier}",
-"DATABASE_USER_NAME" : "${aws_db_instance.this.username}",
-"DATABASE_PASSWORD" : "${random_password.this["rds"].result}",
-"ADMIN_PATH" : "admin_${random_string.this["admin_path"].result}",
-"ADMIN_PASSWORD" : "${random_password.this["magento"].result}",
-"VERSION" : "${var.version}",
-"DOMAIN" : "${var.domain}",
-"BRAND" : "${var.brand}",
-"PHP_USER" : "php-${var.brand}",
-"ADMIN_EMAIL" : "${var.admin_email}",
-"ADMIN_LOGIN" : "${var.admin_login}",
-"ADMIN_FIRSTNAME" : "${var.admin_firstname}",
-"ADMIN_LASTNAME" : "${var.admin_lastname}",
-"LANGUAGE" : "${var.language}",
-"CURRENCY" : "${var.currency}",
-"TIMEZONE" : "${var.timezone}",
-"CACHE_PREFIX" : "${random_string.this["cache_prefix"].result}",
-"SESSION_PERSISTENT" : "${random_string.this["session_persistent"].result}",
-"WEB_ROOT_PATH" : "/home/${var.brand}/public_html",
-"TIMEZONE" : "${var.timezone}",
-"MAGENX_HEADER" : "${random_uuid.this.result}",
-"HEALTH_CHECK_LOCATION" : "${random_string.this["health_check"].result}",
-"MYSQL_PATH" : "mysql_${random_string.this["mysql_path"].result}",
-"PROFILER" : "${random_string.this["profiler"].result}",
-"BLOWFISH" : "${random_password.this["blowfish"].result}",
-"RESOLVER" : "${cidrhost(aws_vpc.this.cidr_block, 2)}",
-"PHP_VERSION" : "${var.php_version}",
-"PHP_INI" : "/etc/php/${var.php_version}/fpm/php.ini",
-"PHP_FPM_POOL" : "/etc/php/${var.php_version}/fpm/pool.d/www.conf",
-"PHP_OPCACHE_INI" : "/etc/php/${var.php_version}/fpm/conf.d/10-opcache.ini",
-"HTTP_X_HEADER" : "${random_uuid.this.result}",
-"LINUX_PACKAGES" : "${var.linux_packages}",
-"PHP_PACKAGES" : "${var.php_packages}",
-"EXCLUDE_LINUX_PACKAGES" : "${var.exclude_linux_packages}",
-"COMPOSER_USER" : "${var.composer_user}",
-"COMPOSER_PASS" : "${var.composer_pass}"
-}
-EOF
-
+  value       = each.value
   tags = {
-    Name = "${local.project}-env"
+    Name = "${local.project}-${local.environment}-${each.key}"
+  }
+}
+locals {
+  parameters = {
+    PROJECT                     = local.project
+    ENVIRONMENT                 = local.environment
+    AWS_DEFAULT_REGION          = data.aws_region.current.name
+    VPC_ID                      = aws_vpc.this.id
+    CIDR                        = aws_vpc.this.cidr_block
+    SUBNET_ID                   = join(",", random_shuffle.subnets.result)
+    EC2_SECURITY_GROUP          = aws_security_group.ec2.id
+    SOURCE_AMI                  = data.aws_ami.distro.id
+    EFS_SYSTEM_ID               = aws_efs_file_system.this.id
+    EFS_ACCESS_POINT_VAR        = aws_efs_access_point.this["var"].id
+    EFS_ACCESS_POINT_MEDIA      = aws_efs_access_point.this["media"].id
+    SNS_TOPIC_ARN               = aws_sns_topic.default.arn
+    FRONTEND_CLOUDMAP_SERVICE_ID = aws_service_discovery_service.this["frontend"].id
+    VARNISH_CLOUDMAP_SERVICE_ID = aws_service_discovery_service.this["varnish"].id
+    RABBITMQ_ENDPOINT           = trimsuffix(trimprefix("${aws_mq_broker.this.instances.0.endpoints.0}", "amqps://"), ":5671")
+    RABBITMQ_USER               = var.brand
+    RABBITMQ_PASSWORD           = random_password.this["rabbitmq"].result
+    OPENSEARCH_ENDPOINT         = "https://${aws_opensearch_domain.this.endpoint}:443"
+    OPENSEARCH_ADMIN            = random_string.this["opensearch"].result
+    OPENSEARCH_PASSWORD         = random_password.this["opensearch"].result
+    REDIS_CACHE_BACKEND         = aws_elasticache_replication_group.this["cache"].primary_endpoint_address
+    REDIS_SESSION_BACKEND       = aws_elasticache_replication_group.this["session"].primary_endpoint_address
+    REDIS_CACHE_BACKEND_RO      = aws_elasticache_replication_group.this["cache"].reader_endpoint_address
+    REDIS_SESSION_BACKEND_RO    = aws_elasticache_replication_group.this["session"].reader_endpoint_address
+    REDIS_PASSWORD              = random_password.this["redis"].result
+    S3_MEDIA_BUCKET             = aws_s3_bucket.this["media"].bucket
+    S3_SYSTEM_BUCKET            = aws_s3_bucket.this["system"].bucket
+    S3_MEDIA_BUCKET_URL         = aws_s3_bucket.this["media"].bucket_regional_domain_name
+    ALB_DNS_NAME                = aws_lb.this.dns_name
+    CLOUDFRONT_DOMAIN           = aws_cloudfront_distribution.this.domain_name
+    SES_KEY                     = aws_iam_access_key.ses_smtp_user_access_key.id
+    SES_SECRET                  = aws_iam_access_key.ses_smtp_user_access_key.secret
+    SES_PASSWORD                = aws_iam_access_key.ses_smtp_user_access_key.ses_smtp_password_v4
+    SES_ENDPOINT                = "email-smtp.${data.aws_region.current.name}.amazonaws.com"
+    MARIADB_ENDPOINT            = aws_db_instance.this.endpoint
+    MARIADB_NAME                = aws_db_instance.this.identifier
+    MARIADB_USER                = aws_db_instance.this.username
+    MARIADB_PASSWORD            = random_password.this["mariadb"].result
+    MARIADB_ROOT_PASSWORD       = random_password.this["mariadb_root"].result
+    ADMIN_PATH                  = "admin_${random_string.this["admin_path"].result}"
+    DOMAIN                      = var.domain
+    BRAND                       = var.brand
+    PHP_USER                    = "php-${var.brand}"
+    ADMIN_EMAIL                 = var.admin_email
+    WEB_ROOT_PATH               = "/home/${var.brand}/public_html"
+    SECURITY_HEADER             = random_uuid.this.result
+    HEALTH_CHECK_LOCATION       = random_string.this["health_check"].result
+    RESOLVER                    = cidrhost(aws_vpc.this.cidr_block, 2)
+    HTTP_X_HEADER               = random_uuid.this.result
   }
 }
