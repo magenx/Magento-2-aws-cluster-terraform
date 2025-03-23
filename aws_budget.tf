ad811e5f4ce64af828b2126a88a3128e44cb39d0
MagenX <magenx@users.noreply.github.com>
2025-03-23 14:27:42 +0100

    Update aws_budget.tf

--- aws_budget.tf
+++ aws_budget.tf
@@ -15,10 +15,47 @@ resource "aws_budgets_budget" "all" {
 
   notification {
     comparison_operator        = "GREATER_THAN"
+    threshold                  = 75
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_sns_topic_arns  = [aws_sns_topic.default.arn]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
     threshold                  = 100
     threshold_type             = "PERCENTAGE"
     notification_type          = "FORECASTED"
-    subscriber_email_addresses = [var.admin_email]
+    subscriber_sns_topic_arns  = [aws_sns_topic.default.arn]
   }
 }
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create alert when your Cost Anomaly Detection trigger changes
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_ce_anomaly_monitor" "cost" {
+  name              = "${local.project}-cost-anomaly-detection"
+  monitor_type      = "DIMENSIONAL"
+  monitor_dimension = "SERVICE"
+  tags = {
+    Name = "${local.project}-cost-anomaly-detection"
+    }
+}
 
+resource "aws_ce_anomaly_subscription" "cost_alert" {
+  name      = "${local.project}-cost-anomaly-alert"
+  frequency = "IMMEDIATE"
+  threshold_expression {
+      dimension {
+        key           = "ANOMALY_TOTAL_IMPACT_PERCENTAGE"
+        match_options = ["GREATER_THAN_OR_EQUAL"]
+        values        = ["15"]
+      }
+    }
+  monitor_arn_list = [
+    aws_ce_anomaly_monitor.cost.arn
+  ]
+  subscriber {
+    type    = "SNS"
+    address = aws_sns_topic.default.arn
+  }
+}
