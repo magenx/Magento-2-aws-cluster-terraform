876a4034c17389b9bc6eb3efcc3b4a975305a030
MagenX <magenx@users.noreply.github.com>
2025-03-25 13:28:27 +0100

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,7 +1,7 @@
 
 
 
-///////////////////////////////////////////////////[ AWS CERTIFICATE MANAGER ]////////////////////////////////////////////
+/////////////////////////////////////////////////////[ AWS ECS CONFIGURATION ]////////////////////////////////////////////
 
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create ecs cluster
@@ -92,4 +92,74 @@ resource "aws_ecs_task_definition" "this" {
 resource "aws_cloudwatch_log_group" "ecs" {
   name              = "/${local.project}/ecs/"
   retention_in_days = 90
+}
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create role to manage the ECS Cluster
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_role" "ecs_service_role" {
+  name               = "${local.project}_ECSServiceRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_service_policy.json
+}
+data "aws_iam_policy_document" "ecs_service_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs.amazonaws.com",]
+    }
+  }
+}
+resource "aws_iam_role_policy" "ecs_service_role_policy" {
+  name   = "${local.project}_ECSServiceRolePolicy"
+  policy = data.aws_iam_policy_document.ecs_service_role_policy.json
+  role   = aws_iam_role.ecs_service_role.id
+}
+data "aws_iam_policy_document" "ecs_service_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:Describe*",
+      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+      "elasticloadbalancing:DeregisterTargets",
+      "elasticloadbalancing:Describe*",
+      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+      "elasticloadbalancing:RegisterTargets",
+      "ec2:DescribeTags",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:DescribeLogStreams",
+      "logs:PutSubscriptionFilter",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+}
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create role for ECS Task execution
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "${local.project}_ECSTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.task_assume_role_policy.json
+}
+data "aws_iam_policy_document" "task_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+resource "aws_iam_role" "ecs_task_iam_role" {
+  name               = "${local.project}_ECSTaskIAMRole"
+  assume_role_policy = data.aws_iam_policy_document.task_assume_role_policy.json
 }
